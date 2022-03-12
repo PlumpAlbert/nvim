253b4ca4c7b60abdce5cbe0fe15702c7590e678d
plump_albert <plumpalbert@gmail.com>
2022-03-12 10:45:28 +0300

    Different theming per OS

--- init.vim
+++ init.vim
@@ -1,4 +1,14 @@
 " vim:ft=vim:ts=4:sw=0
+syntax enable
+set background=dark
+let g:everforest_background = 'hard'
+let g:oceanic_next_terminal_bold = 1
+let g:oceanic_next_terminal_italic = 0
+let g:gruvbox_material_palette = 'material'
+let g:gruvbox_material_background = 'hard'
+let g:gruvbox_material_disable_italic_comment = 1
+let g:gruvbox_material_ui_contrast = 'high'
+
 if has('win32')
 	hi Normal guibg=NONE ctermbg=NONE
 	hi LineNr guibg=NONE ctermbg=NONE
@@ -9,20 +19,22 @@ if has('win32')
 	let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
 	set shellquote= shellxquote=
 	let &shellcmdflag='-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
+	" Theming
+	let g:gruvbox_material_palette = 'mix'
+	let g:gruvbox_material_transparent_background = 1
+	colorscheme gruvbox-material
+else
+	" colorscheme everforest
+	" colorscheme afterglow
+	" colorscheme nord
+	" colorscheme dracula
+	" colorscheme gruvbox-material
+	colorscheme OceanicNext
 endif
+
 if has('termguicolors')
 	set termguicolors
 endif
-syntax enable
-set background=dark
-let g:everforest_background = 'hard'
-let g:oceanic_next_terminal_bold = 1
-let g:oceanic_next_terminal_italic = 0
-" colorscheme everforest
-" colorscheme afterglow
-" colorscheme nord
-" colorscheme dracula
-colorscheme OceanicNext
 
 
 let mapleader=' '
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,33 +2,33 @@
 -- Author: Plump Albert (plumpalbert@gmail.com)
 
 require('lualine').setup {
-  options = {
-    theme = 'OceanicNext',
-    icons_enabled = true,
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {},
-    always_divide_middle = true,
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff'},
-    lualine_c = {'filename'},
-    lualine_x = {'filetype'},
-    lualine_y = {'diagnostics'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  extensions = {
-	  'nvim-tree',
-	  'toggleterm'
-  }
+	options = {
+		theme = vim.fn.has('win32') and 'gruvbox-material' or 'OceanicNext',
+		icons_enabled = true,
+		component_separators = { left = '', right = ''},
+		section_separators = { left = '', right = ''},
+		disabled_filetypes = {},
+		always_divide_middle = true,
+	},
+	sections = {
+		lualine_a = {'mode'},
+		lualine_b = {'branch', 'diff'},
+		lualine_c = {'filename'},
+		lualine_x = {'filetype'},
+		lualine_y = {'diagnostics'},
+		lualine_z = {'location'}
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = {'filename'},
+		lualine_x = {'location'},
+		lualine_y = {},
+		lualine_z = {}
+	},
+	tabline = {},
+	extensions = {
+		'nvim-tree',
+		'toggleterm'
+	}
 }
