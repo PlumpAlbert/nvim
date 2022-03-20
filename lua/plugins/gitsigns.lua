edd21b479026cb28a1f7f4b967715dadf7a0eeeb
plump_albert <plumpalbert@gmail.com>
2022-03-20 12:47:55 +0300

    Updated character for displaying in gutter

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,10 +2,10 @@
 local gs = require('gitsigns')
 gs.setup {
 	signs = {
-		add			 = {hl = 'GitSignsAdd'	 , text = '┃', numhl='GitSignsAddNr'	 , linehl='GitSignsAddLn'},
-		change		 = {hl = 'GitSignsChange', text = '┃', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-		delete		 = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-		topdelete	 = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+		add = {hl = 'GitSignsAdd'	 , text = '│', numhl='GitSignsAddNr'	 , linehl='GitSignsAddLn'},
+		change = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+		delete = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+		topdelete = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
 		changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
 	},
 	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
