23acc2c859ccf0f7f42487ae976b02b6076aaaa0
plump_albert <plumpalbert@gmail.com>
2022-03-26 11:32:10 +0300

    Highlight modified line numbers

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,15 +1,15 @@
--- vim:ft=lua:ts=4:sw=0
+-- vim:ft=lua:ts=2:sw=0
 local gs = require('gitsigns')
 gs.setup {
 	signs = {
-		add = {hl = 'GitSignsAdd'	 , text = '│', numhl='GitSignsAddNr'	 , linehl='GitSignsAddLn'},
+		add = {hl = 'GitSignsAdd', text = '│', numhl='GitSignsAddNr' , linehl='GitSignsAddLn'},
 		change = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
 		delete = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
 		topdelete = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
 		changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
 	},
 	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-	numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+	numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
 	linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
 	word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
 	watch_gitdir = {
