decfc101eb36602e6d381bfbb8162f609015b372
plump_albert <plumpalbert@gmail.com>
2022-03-27 15:09:59 +0300

    Added additional folders for exclusion in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -37,7 +37,12 @@ local default = {
 			preview_cutoff = 120,
 		},
 		file_sorter = require("telescope.sorters").get_fuzzy_file,
-		file_ignore_patterns = { "node_modules" },
+		file_ignore_patterns = {
+			"node_modules",
+			"vendor",
+			"build",
+			".venv"
+		},
 		generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
 		path_display = { "truncate" },
 		winblend = 0,
