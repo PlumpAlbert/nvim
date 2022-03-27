4bfde1f38639e7572a2456444f9018fd46695551
plump_albert <plumpalbert@gmail.com>
2022-03-27 15:11:04 +0300

    Highlight only changed line numbers

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -8,7 +8,7 @@ gs.setup {
 		topdelete = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
 		changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
 	},
-	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+	signcolumn = false, -- Toggle with `:Gitsigns toggle_signs`
 	numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
 	linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
 	word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
