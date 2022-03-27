50d2ad9177c2d677ff775a2b65f34bf5a20abe0e
plump_albert <plumpalbert@gmail.com>
2022-03-27 15:12:17 +0300

    Using default capabilities and on_attach function

--- lua/plugins/lsp/intelephense.lua
+++ lua/plugins/lsp/intelephense.lua
@@ -2,6 +2,12 @@ local LSP = require "plugins.lsp.init"
 local lspconfig = require "lspconfig"
 
 lspconfig.intelephense.setup {
+	capabilities = LSP.capabilities,
+	on_attach = function(client, ...)
+		client.resolved_capabilities.document_formatting = false
+		client.resolved_capabilities.document_range_formatting = false
+		LSP.on_attach(client, ...)
+	end,
 	filetypes = {
 		'php',
 		'blade'
