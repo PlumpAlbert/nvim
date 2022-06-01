eaf72e8a4a551b37d143028a2057587e467f5d31
plump_albert <plumpalbert@gmail.com>
2022-06-01 10:33:36 +0300

    added lazygit integration

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,3 +1,17 @@
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>lg",
+	"<cmd>lua _lazygit_toggle()<CR>",
+	{noremap = true, silent = true}
+)
+
 require("toggleterm").setup {
 	-- size can be a number or function which is passed the current terminal
 	size = function(term)
