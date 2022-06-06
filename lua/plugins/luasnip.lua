4683321a4882c62b6c68d4829c86abdd112957ce
plump_albert <plumpalbert@gmail.com>
2022-06-06 18:48:43 +0300

    updated `rfc` snippet

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -17,20 +17,20 @@ local fmt = require("luasnip.extras.fmt").fmt
 
 -- Config for luasnip
 ls.config.set_config {
-	updateevents = "TextChanged,TextChangedI",
+    updateevents = "TextChanged,TextChangedI",
 }
 
 function dump(o)
-	if type(o) == 'table' then
-		local s = '{ '
-		for k, v in pairs(o) do
-			if type(k) ~= 'number' then k = '"' .. k .. '"' end
-			s = s .. '[' .. k .. '] = ' .. dump(v) .. ','
-		end
-		return s .. '} '
-	else
-		return tostring(o)
-	end
+    if type(o) == 'table' then
+        local s = '{ '
+        for k, v in pairs(o) do
+            if type(k) ~= 'number' then k = '"' .. k .. '"' end
+            s = s .. '[' .. k .. '] = ' .. dump(v) .. ','
+        end
+        return s .. '} '
+    else
+        return tostring(o)
+    end
 end
 
 -- Shorthands
@@ -39,73 +39,73 @@ local BS = function(args, snip) return snip.env.BLOCK_COMMENT_START end
 local BE = function(args, snip) return snip.env.BLOCK_COMMENT_END end
 
 ls.add_snippets("all", {
-	s({
-		trig = "@copy",
-		name = "Copyright",
-		dscr = "Insert copyright data with vim magic function"
-	}, fmt(
-		'{} vim:ft={}:ts={}:sw=0\n{} Author: Plump Albert (plumpalbert@gmail.com)',
-		{ f(LC), i(1, vim.bo.filetype), i(2, tostring(vim.bo.tabstop)), f(LC) }
-	))
+    s({
+        trig = "@copy",
+        name = "Copyright",
+        dscr = "Insert copyright data with vim magic function"
+    }, fmt(
+        '{} vim:ft={}:ts={}:sw=0\n{} Author: Plump Albert (plumpalbert@gmail.com)',
+        { f(LC), i(1, vim.bo.filetype), i(2, tostring(vim.bo.tabstop)), f(LC) }
+    ))
 })
 ls.add_snippets("tex", {
-	s(
-		{
-			trig = "fig",
-			dscr = "Creates block for inserting image"
-		},
-		{
-			t("\\begin{figure}["), i(1, "H"), t({
-				"]",
-				"\\centering",
-				"\\includegraphics[keepaspectratio,width=\\textwidth]{"
-			}), i(2, 'image'), t({
-				"}",
-				"\\caption{"
-			}), i(3, 'Image caption'), t({
-				"}",
-				"\\label{fig:"
-			}), i(0), t({
-				"}",
-				"\\end{figure}"
-			})
-		}
-	)
+    s(
+        {
+            trig = "fig",
+            dscr = "Creates block for inserting image"
+        },
+        {
+            t("\\begin{figure}["), i(1, "H"), t({
+                "]",
+                "\\centering",
+                "\\includegraphics[keepaspectratio,width=\\textwidth]{"
+            }), i(2, 'image'), t({
+                "}",
+                "\\caption{"
+            }), i(3, 'Image caption'), t({
+                "}",
+                "\\label{fig:"
+            }), i(0), t({
+                "}",
+                "\\end{figure}"
+            })
+        }
+    )
 })
 
 ls.add_snippets("tex", {
-	s(
-		{
-			trig = '"',
-			dscr = "Enquote text"
-		},
-		{
-			t("\\textquote{"), i(0, "text"), t('}'),
-		}
-	)
+    s(
+        {
+            trig = '"',
+            dscr = "Enquote text"
+        },
+        {
+            t("\\textquote{"), i(0, "text"), t('}'),
+        }
+    )
 })
 
-local bufferName = function()
-	return string.match(vim.fn.expand('%:t'), '[^.]+')
+local bufferName = function(_, snip)
+    return snip.env.TM_FILENAME_BASE
 end
 
 ls.add_snippets("javascriptreact", {
-	s(
-		{ trig = 'rfc', dscr = 'Create React Functional component' },
-		{
-			t({ 'import React from "react";' }),
-			t({ '', "const " }), f(bufferName), t(" = ({"),
-				i(1, "...props"),
-			t({
-				"}) => {",
-				"\treturn null;",
-				"}",
-			}),
-			t({'', 'export default '}),
-			f(bufferName),
-			i(0)
-		}
-	)
+    s(
+        { trig = 'rfc', dscr = 'Create React Functional component' },
+        {
+            t({ 'import React from "react";', '' }),
+            t({ '', "const " }), f(bufferName), t(" = ({"),
+            i(1, "...props"),
+            t({ "}) => {", '' }),
+            i(0),
+            t({ '', "\treturn null;", '' }),
+            t({ "}",
+                ""
+            }),
+            t({ '', 'export default ' }),
+            f(bufferName),
+        }
+    )
 })
 
 return ls
