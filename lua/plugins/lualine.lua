f1186dcbbfbcb332852e317357b91d1fea618a7c
plump_albert <plumpalbert@gmail.com>
2022-06-11 22:34:27 +0300

    updated lualine configuration

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,70 +5,69 @@
 
 -- stylua: ignore
 local colors = {
-	blue   = '#80a0ff',
-	cyan   = '#79dac8',
-	black  = '#080808',
-	white  = '#c6c6c6',
-	red    = '#ff5189',
-	violet = '#d183e8',
-	grey   = '#303030',
+    blue   = '#80a0ff',
+    cyan   = '#79dac8',
+    black  = '#080808',
+    white  = '#c6c6c6',
+    red    = '#ff5189',
+    violet = '#d183e8',
+    grey   = '#303030',
 }
 
 local bubbles_theme = {
-	normal = {
-		a = { fg = colors.black, bg = colors.violet },
-		b = { fg = colors.white, bg = colors.grey },
-		c = { fg = colors.grey, bg = colors.black },
-	},
+    normal = {
+        a = { fg = colors.black, bg = colors.violet },
+        b = { fg = colors.white, bg = colors.grey },
+        c = { fg = colors.grey, bg = colors.black },
+    },
 
-	insert = { a = { fg = colors.black, bg = colors.blue } },
-	visual = { a = { fg = colors.black, bg = colors.cyan } },
-	replace = { a = { fg = colors.black, bg = colors.red } },
+    insert = { a = { fg = colors.black, bg = colors.blue } },
+    visual = { a = { fg = colors.black, bg = colors.cyan } },
+    replace = { a = { fg = colors.black, bg = colors.red } },
 
-	inactive = {
-		a = { fg = colors.white, bg = colors.black },
-		b = { fg = colors.white, bg = colors.black },
-		c = { fg = colors.black, bg = colors.black },
-	},
+    inactive = {
+        a = { fg = colors.white, bg = colors.black },
+        b = { fg = colors.white, bg = colors.black },
+        c = { fg = colors.black, bg = colors.black },
+    },
 }
 
 require('lualine').setup {
-	options = {
-		theme = bubbles_theme,
-		component_separators = { left = '/', right = '\\' },
-		section_separators = { left = '', right = '' },
-		disabled_filetypes = {
-			'TelescopePrompt',
-			'NvimTree',
-			'packer',
-		},
-	},
-	sections = {
-		lualine_a = {
-			{ 'mode', separator = { left = '' }, right_padding = 2 },
-		},
-		lualine_b = { 'filename', 'branch' },
-		lualine_c = {},
-		lualine_x = {},
-		lualine_y = {
-			'filetype',
-			{'fileformat',
-				separator = { right = '' },
-				left_padding = 2
-			}
-		},
-		lualine_z = {},
-	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = {},
-		lualine_x = {},
-		lualine_y = {},
-		lualine_z = {},
-	},
-	tabline = {},
-	extensions = {},
+    options = {
+        theme = 'codedark',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+        disabled_filetypes = {
+            'TelescopePrompt',
+            'NvimTree',
+            'packer',
+        },
+    },
+    sections = {
+        lualine_a = {
+            { 'mode', right_padding = 2 },
+        },
+        lualine_b = { 'filename', 'branch' },
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {
+            'filetype',
+            'fileformat',
+        },
+        lualine_z = {},
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+    },
+    tabline = {},
+    extensions = {},
 }
 
-vim.cmd "set laststatus=3"
+vim.cmd "\
+set laststatus=3\
+hi WinSeparator ctermfg=none guifg=none"
