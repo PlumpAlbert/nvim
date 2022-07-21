518e94696192508f23bda6992c67c124726a44da
plump_albert <plumpalbert@gmail.com>
2022-07-21 13:46:36 +0300

    updated script for installing servers

--- lsp-servers.sh
+++ lsp-servers.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
-echo '# Installing language servers'
-npm i -g typescript \
-	typescript-language-server \
-	vscode-langservers-extracted \
-	@tailwindcss/language-server \
-	@kozer/emmet-language-server \
-	intelephense \
-	pyright \
-	prettier \
-	blade-formatter \
-	dockerfile-language-server-nodejs
+echo '# Installing null_ls servers'
+npm i -g cspell \
+  @cspell/dict-ru_ru \
+  @kozer/emmet-language-server \
+  @fsouza/prettierd \
+  eslint_d
 
 echo '# Downloading phpcbf'
 ! [ -d "$HOME/.local/bin" ] && mkdir -p "$HOME/.local/bin"
@@ -18,7 +13,8 @@ chmod a+x "$HOME/.local/bin/phpcs"
 curl -sL 'https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar' -o "$HOME/.local/bin/phpcbf"
 chmod a+x "$HOME/.local/bin/phpcbf"
 
-echo '# Installing clangd and shfmt'
-sudo -S pacman -Sy --needed --noconfirm cmake clang llvm shfmt
-echo '# Installing cmake-language-server'
-pip install --user cmake-language-server
+which pacman >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+  echo '# Installing clangd and shfmt'
+  sudo -S pacman -Sy --needed --noconfirm cmake clang llvm shfmt
+fi
