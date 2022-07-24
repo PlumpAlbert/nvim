7b40f58c469db931e6abd95df446513541434c8f
plump_albert <plumpalbert@gmail.com>
2022-07-24 09:46:30 +0300

    updated lua lsp server config

--- lua/plugins/lsp/settings/Lua.lua
+++ lua/plugins/lsp/settings/Lua.lua
@@ -1,7 +1,27 @@
 local M = {
-	diagnostics = {
-		globals = { "vim" },
-	}
-}
-
+  settings = {
+    Lua = {
+      workspace = {
+        library = {
+          ['/usr/share/nvim/runtime/lua'] = true,
+          ['/usr/share/nvim/runtime/lua/vim'] = true,
+          ['/usr/share/nvim/runtime/lua/vim/lsp'] = true,
+          ['/usr/share/awesome/lib'] = true
+        }
+      };
+      diagnostics = {
+        enable = true;
+        globals = {
+          -- VIM
+          "vim",
+          "use", -- Packer use keyword
+          -- AwesomeWM
+          "awesome",
+          "client",
+          "root"
+        }
+      }
+    }
+  }
+};
 return M
