690ceb67658c07b28efedaf7df6e1519c11640d9
plump_albert <plumpalbert@gmail.com>
2022-08-07 10:12:25 +0300

    updated colorizer plugin

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -2,29 +2,18 @@
 local present, colorizer = pcall(require, "colorizer")
 if present then
     local default = {
-        filetypes = {
-            "css",
-            "scss",
-            "sass",
-            "less",
-            "javascriptreact",
-            "typescriptreact",
-            "lua"
-        },
-        user_default_options = {
-            RGB = true, -- #RGB hex codes
-            RRGGBB = true, -- #RRGGBB hex codes
-            names = false, -- "Name" codes like Blue
-            RRGGBBAA = false, -- #RRGGBBAA hex codes
-            rgb_fn = false, -- CSS rgb() and rgba() functions
-            hsl_fn = false, -- CSS hsl() and hsla() functions
-            css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-            css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+        RGB = true, -- #RGB hex codes
+        RRGGBB = true, -- #RRGGBB hex codes
+        names = false, -- "Name" codes like Blue
+        RRGGBBAA = false, -- #RRGGBBAA hex codes
+        rgb_fn = false, -- CSS rgb() and rgba() functions
+        hsl_fn = false, -- CSS hsl() and hsla() functions
+        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
 
-            -- Available modes: foreground, background
-            mode = "background", -- Set the display mode.
-        },
+        -- Available modes: foreground, background
+        mode = "background", -- Set the display mode.
     }
-    colorizer.setup(default["filetypes"], default["user_default_options"])
+    colorizer.setup { "*"; css = default; html = default; sass = default; }
     vim.cmd "ColorizerReloadAllBuffers"
 end
