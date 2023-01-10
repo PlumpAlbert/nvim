d1956f8fbca89d2e0d5693a5c14a847969471beb
plump_albert <plumpalbert@gmail.com>
2023-01-10 21:25:15 +0300

    updated treesitter dependencies

--- lua/elanianel/plugins/treesitter.lua
+++ lua/elanianel/plugins/treesitter.lua
@@ -1,7 +1,15 @@
 return {
-	{ 'nvim-treesitter/nvim-treesitter', run = 'TSUpdate' },
-	'nvim-treesitter/nvim-treesitter-context',
-        'p00f/nvim-ts-rainbow',
-        'windwp/nvim-ts-autotag',
-        'JoosepAlviste/nvim-ts-context-commentstring',
+  { 'nvim-treesitter/nvim-treesitter',
+    run = 'TSUpdate',
+    dependecies = {
+      'nvim-treesitter/nvim-treesitter-context',
+      'p00f/nvim-ts-rainbow',
+      'windwp/nvim-ts-autotag',
+      'JoosepAlviste/nvim-ts-context-commentstring',
+    }
+  },
+  'nvim-treesitter/nvim-treesitter-context',
+  'p00f/nvim-ts-rainbow',
+  'windwp/nvim-ts-autotag',
+  'JoosepAlviste/nvim-ts-context-commentstring',
 }
