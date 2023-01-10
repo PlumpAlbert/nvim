ae17c7148540e0f1819fa64962c682556a59f2ee
plump_albert <plumpalbert@gmail.com>
2023-01-10 20:56:48 +0300

    moved LSP keys to which key config

--- lua/elanianel/config/lsp.lua
+++ lua/elanianel/config/lsp.lua
@@ -70,27 +70,14 @@ lsp.configure('sumneko_lua', {
 })
 
 lsp.on_attach(function(client, bufnr)
-    local opts = { buffer = bufnr, remap = false }
-
     if client.name == "eslint_d" then
         return
     end
 
-    if client.name ~= 'tsserver' then
+    if client.name == 'tsserver' then
         client.server_capabilities.document_formatting = false
         client.server_capabilities.range_formatting = false
     end
-
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "<leader>lj", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "<leader>lk", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("n", "<leader>la", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -116,9 +103,6 @@ null_ls.setup {
         null_opts.on_attach(client, bufnr)
     end,
     sources = {
-        b.diagnostics.cspell.with {
-            extra_args = { '--config', '~/.config/cspell.json' }
-        },
         b.diagnostics.eslint_d,
         b.code_actions.eslint_d,
         -- prettier
