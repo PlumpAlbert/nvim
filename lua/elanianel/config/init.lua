aa154e00abd2ecf30348c2baf88d0b2ac2386324
plump_albert <plumpalbert@gmail.com>
2023-01-10 21:08:08 +0300

    added config for neotree

--- lua/elanianel/config/init.lua
+++ lua/elanianel/config/init.lua
@@ -1,2 +1,3 @@
 local configPath = 'elanianel.config'
 require (configPath..".lsp")
+require (configPath..".neotree")
---
+++ lua/elanianel/config/neotree.lua
@@ -0,0 +1,11 @@
+require'neo-tree'.setup {
+  window = {
+    position = 'right',
+    width = 25,
+  },
+  filesystem = {
+    filtered_items = {
+      hide_gitignored = true,
+    }
+  }
+}
