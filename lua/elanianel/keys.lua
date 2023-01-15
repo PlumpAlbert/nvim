f4ec2b1c045875fc36e7edbef35d643f5e0d1885
plump_albert <plumpalbert@gmail.com>
2023-01-15 10:34:55 +0300

    updated which-key config

--- lua/elanianel/keys.lua
+++ lua/elanianel/keys.lua
@@ -7,7 +7,18 @@ presets.operators['zM'] = nil
 presets.operators['zR'] = nil
 
 local wk = require 'which-key'
-wk.setup {}
+wk.setup {
+    presets = {
+        g = false,
+        text_objects = false,
+    },
+    triggers_blacklist = {
+        i = { 's' }
+    },
+    disable = {
+        filetypes = { "TelescopePrompt" }
+    }
+}
 
 -- leader
 wk.register({
@@ -77,7 +88,30 @@ wk.register({
     },
     f = { require 'telescope.builtin'.git_files, 'Find Git files' },
     r = { require 'ssr'.open, 'Find and replace', mode = { 'n', 'x' } },
-    c = { vim.cmd.BufferClose, 'Close buffer' }
+    c = { vim.cmd.BufferClose, 'Close buffer' },
+    d = {
+        name = "Debug",
+        R = { "<cmd>lua require'dap'.run_to_cursor()<cr>", "Run to Cursor" },
+        E = { "<cmd>lua require'dapui'.eval(vim.fn.input '[Expression] > ')<cr>", "Evaluate Input" },
+        C = { "<cmd>lua require'dap'.set_breakpoint(vim.fn.input '[Condition] > ')<cr>", "Conditional Breakpoint" },
+        U = { "<cmd>lua require'dapui'.toggle()<cr>", "Toggle UI" },
+        b = { "<cmd>lua require'dap'.step_back()<cr>", "Step Back" },
+        c = { "<cmd>lua require'dap'.continue()<cr>", "Continue" },
+        d = { "<cmd>lua require'dap'.disconnect()<cr>", "Disconnect" },
+        e = { "<cmd>lua require'dapui'.eval()<cr>", "Evaluate" },
+        g = { "<cmd>lua require'dap'.session()<cr>", "Get Session" },
+        h = { "<cmd>lua require'dap.ui.widgets'.hover()<cr>", "Hover Variables" },
+        S = { "<cmd>lua require'dap.ui.widgets'.scopes()<cr>", "Scopes" },
+        i = { "<cmd>lua require'dap'.step_into()<cr>", "Step Into" },
+        o = { "<cmd>lua require'dap'.step_over()<cr>", "Step Over" },
+        p = { "<cmd>lua require'dap'.pause.toggle()<cr>", "Pause" },
+        q = { "<cmd>lua require'dap'.close()<cr>", "Quit" },
+        r = { "<cmd>lua require'dap'.repl.toggle()<cr>", "Toggle Repl" },
+        s = { "<cmd>lua require'dap'.continue()<cr>", "Start" },
+        t = { "<cmd>lua require'dap'.toggle_breakpoint()<cr>", "Toggle Breakpoint" },
+        x = { "<cmd>lua require'dap'.terminate()<cr>", "Terminate" },
+        u = { "<cmd>lua require'dap'.step_out()<cr>", "Step Out" },
+    },
 }, { prefix = '<leader>' })
 
 -- global
@@ -89,11 +123,11 @@ wk.register({
     ['zM'] = { require 'ufo'.closeAllFolds, 'Close all folds' },
     ['zR'] = { require 'ufo'.openAllFolds, 'Open all folds' },
     ['<M-h>'] = {
-        function() require'bufferline.api'.goto_buffer_relative(-1) end,
+        function() require 'bufferline.api'.goto_buffer_relative(-1) end,
         'Go to previous buffer'
     },
     ['<M-l>'] = {
-        function() require'bufferline.api'.goto_buffer_relative(1) end,
+        function() require 'bufferline.api'.goto_buffer_relative(1) end,
         'Go to next buffer'
     },
     g = {
