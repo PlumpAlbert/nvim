49a7ea4275962754a2b804ce8668a0f6e6950617
plump_albert <plumpalbert@gmail.com>
2023-01-16 17:27:19 +0300

    loading treesitter plugins

--- lua/elanianel/plugins/treesitter.lua
+++ lua/elanianel/plugins/treesitter.lua
@@ -8,8 +8,8 @@ return {
       'JoosepAlviste/nvim-ts-context-commentstring',
     }
   },
-  'nvim-treesitter/nvim-treesitter-context',
-  'p00f/nvim-ts-rainbow',
-  'windwp/nvim-ts-autotag',
-  'JoosepAlviste/nvim-ts-context-commentstring',
+  { 'nvim-treesitter/nvim-treesitter-context', lazy = false },
+  { 'p00f/nvim-ts-rainbow', lazy = false },
+  { 'windwp/nvim-ts-autotag', lazy = false },
+  { 'JoosepAlviste/nvim-ts-context-commentstring', lazy = false },
 }
