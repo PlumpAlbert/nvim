270c89125bee3d18a0ea929dd70547efe8cbf4a9
plump_albert <plumpalbert@gmail.com>
2023-01-19 09:50:57 +0300

    set fileformat to dos on windows

--- lua/elanianel/windows.lua
+++ lua/elanianel/windows.lua
@@ -6,5 +6,5 @@ vim.opt.shellxquote = nil
 vim.opt.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
 vim.opt.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
 vim.opt.shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
-vim.opt.fileformat = 'unix'
+vim.opt.fileformat = 'dos'
 vim.opt.guifont = 'JetBrainsMono NF:h11'
