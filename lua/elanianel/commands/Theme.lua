d366f16dd1059f68229ad4a1e5c99cb23741598e
plump_albert <plumpalbert@gmail.com>
2023-02-08 14:58:51 +0300

    added custom command to apply random colorscheme

--- lua/elanianel/commands/Theme.lua
+++ lua/elanianel/commands/Theme.lua
@@ -1,8 +1,14 @@
 -- vim:ft=lua:ts=4:sw=0
-local themes = { 'gruvbox', 'palenightfall', 'catppuccin', 'github' }
+local themes = {
+    'gruvbox',
+    'catppuccin',
+    'github',
+    'everforest',
+    'moonfly',
+}
 local themes_dir = 'elanianel.config.themes.'
 
-vim.api.nvim_create_user_command('Pretty', function()
+vim.api.nvim_create_user_command('Theme', function()
     local theme_id = math.random(1, #themes)
     require(themes_dir .. themes[theme_id])
 end, {})
