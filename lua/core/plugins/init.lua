6a1114b9a0ccc30a6692c334652288f26d4a1e5d
plump_albert <plumpalbert@gmail.com>
2023-02-11 16:03:58 +0300

    created DAP config

--- lua/core/plugins/init.lua
+++ lua/core/plugins/init.lua
@@ -6,5 +6,6 @@ M = utils.concat(M, require "core.plugins.ui")
 M = utils.concat(M, require "core.plugins.ux")
 M = utils.concat(M, require "core.plugins.telescope")
 M = utils.concat(M, require "core.plugins.treesitter")
+M = utils.concat(M, require "core.plugins.dap")
 
 return M
