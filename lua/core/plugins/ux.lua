e10f1a5c7f080730da5f0e34068732f33c50994d
plump_albert <plumpalbert@gmail.com>
2023-02-18 08:49:37 +0300

    added guess-indent.nvim plugin

--- lua/core/plugins/ux.lua
+++ lua/core/plugins/ux.lua
@@ -1,66 +1,86 @@
 return {
-    { "folke/which-key.nvim",
-        lazy = false,
-        config = function(_, keys)
-          local wk = require "core.config.which_key"
-          if keys ~= nil then
-            wk.register(keys)
-          end
-        end
+  { "folke/which-key.nvim",
+    lazy = false,
+    config = function(_, keys)
+      local wk = require "core.config.which_key"
+      if keys ~= nil then
+        wk.register(keys)
+      end
+    end
+  },
+  { 'echasnovski/mini.pairs',
+    lazy = false,
+    config = function(_, opts)
+      require "mini.pairs".setup(opts)
+    end
+  },
+  { 'echasnovski/mini.comment',
+    lazy = false,
+    config = function(_, opts)
+      require "mini.comment".setup(opts)
+    end
+  },
+  { 'echasnovski/mini.surround',
+    lazy = false,
+    opts = {
+      mappings = {
+        add = '<leader>ya', -- Add surrounding in Normal and Visual modes
+        delete = '<leader>yd', -- Delete surrounding
+        replace = '<leader>yc', -- Replace surrounding
+        find = '', -- Find surrounding (to the right)
+        find_left = '', -- Find surrounding (to the left)
+        highlight = '', -- Highlight surrounding
+        update_n_lines = '', -- Update `n_lines`
+        suffix_last = '', -- Suffix to search with "prev" method
+        suffix_next = '', -- Suffix to search with "next" method
+      },
     },
-    { 'echasnovski/mini.pairs',
-        lazy = false,
-        config = function(_, opts)
-          require "mini.pairs".setup(opts)
-        end
+    config = function(_, opts)
+      require "mini.surround".setup(opts)
+    end
+  },
+  { "windwp/nvim-spectre",
+    keys = {
+      { '<leader>R',
+        function()
+          require "spectre".open_visual { select_word = true }
+        end,
+        desc = "Find and replace",
+        mode = { "n", "x" }
+      },
     },
-    { 'echasnovski/mini.comment',
-        lazy = false,
-        config = function(_, opts)
-          require "mini.comment".setup(opts)
-        end
-    },
-    { 'echasnovski/mini.surround',
-        lazy = false,
-        opts = {
-            mappings = {
-                add = '<leader>ya', -- Add surrounding in Normal and Visual modes
-                delete = '<leader>yd', -- Delete surrounding
-                replace = '<leader>yc', -- Replace surrounding
-                find = '', -- Find surrounding (to the right)
-                find_left = '', -- Find surrounding (to the left)
-                highlight = '', -- Highlight surrounding
-                update_n_lines = '', -- Update `n_lines`
-                suffix_last = '', -- Suffix to search with "prev" method
-                suffix_next = '', -- Suffix to search with "next" method
-            },
+    opts = {
+      min_width = 30,
+      min_height = 5,
+      max_width = 70,
+      max_height = 12,
+      keymaps = {
+        close = 'q',
+        next_match = 'n',
+        prev_match = 'N',
+        replace_confirm = '<cr>',
+        replace_all = '<leader><cr>',
+      }
+    }
+  },
+  { "nmac427/guess-indent.nvim",
+    event = "BufEnter",
+    config = function()
+      require('guess-indent').setup {
+        auto_cmd = true, -- Set to false to disable automatic execution
+        filetype_exclude = { -- A list of filetypes for which the auto command gets disabled
+          "netrw",
+          "tutor",
+          "neo-tree",
+          "TelescopePrompt",
         },
-        config = function(_, opts)
-          require "mini.surround".setup(opts)
-        end
-    },
-    { "windwp/nvim-spectre",
-        keys = {
-            { '<leader>R',
-                function()
-                  require "spectre".open_visual { select_word = true }
-                end,
-                desc = "Find and replace",
-                mode = { "n", "x" }
-            },
+        buftype_exclude = { -- A list of buffer types for which the auto command gets disabled
+          "help",
+          "nofile",
+          "terminal",
+          "prompt",
         },
-        opts = {
-            min_width = 30,
-            min_height = 5,
-            max_width = 70,
-            max_height = 12,
-            keymaps = {
-                close = 'q',
-                next_match = 'n',
-                prev_match = 'N',
-                replace_confirm = '<cr>',
-                replace_all = '<leader><cr>',
-            }
-        }
-    },
+      }
+    end
+  }
 }
