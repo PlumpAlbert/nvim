71386461bcc7105bb844f839363a12ee46220cb4
plump_albert <plumpalbert@gmail.com>
2023-02-20 16:31:37 +0300

    moved lazy status to lualine_x

--- lua/core/config/lualine.lua
+++ lua/core/config/lualine.lua
@@ -4,14 +4,14 @@ return {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff' },
         lualine_c = { 'filename' },
-        lualine_x = {},
-        lualine_y = {
+        lualine_x = {
             {
                 require 'lazy.status'.updates,
                 cond = require 'lazy.status'.has_updates,
                 color = { fg = '#ff9e64' }
             }
         },
+        lualine_y = {},
         lualine_z = { 'diagnostics', 'filetype' },
     },
     extensions = {
