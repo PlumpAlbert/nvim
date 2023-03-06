c99aeb4aa00c9772a1206b293c9294fefb59b5d5
plump_albert <plumpalbert@gmail.com>
2023-03-06 16:04:03 +0300

    created custom config for nvim-cmp

---
+++ lua/_core/lsp/completion.lua
@@ -0,0 +1,86 @@
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+local lspkind = require("lspkind")
+
+local completeKeymap = vim.fn.has("win32") == 1 and "<C-p>" or "<C-Space>"
+
+local mappings = {
+  ["<C-k>"] = cmp.mapping(function(fallback)
+    if cmp.visible() then
+      cmp.select_prev_item()
+    elseif luasnip.jumpable(-1) then
+      luasnip.jump(-1)
+    else
+      fallback()
+    end
+  end, { "i", "s" }),
+  ["<C-j>"] = cmp.mapping(function(fallback)
+    if cmp.visible() then
+      cmp.select_next_item()
+    elseif luasnip.expand_or_locally_jumpable() then
+      luasnip.expand_or_jump()
+    else
+      fallback()
+    end
+  end, { "i", "s" }),
+  ["<CR>"] = cmp.mapping.confirm({ behaviour = cmp.SelectBehavior.Select, select = true }),
+  [completeKeymap] = cmp.mapping.complete({}),
+}
+
+cmp.setup({
+  window = {
+    completion = {
+      winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+      col_offset = -3,
+      side_padding = 0,
+    },
+  },
+  view = { entries = "custom", selection_order = "near_cursor" },
+  formatting = {
+    fields = { "kind", "abbr", "menu" },
+    format = function(entry, vim_item)
+      local kind = lspkind.cmp_format({
+        mode = "symbol_text",
+        maxwidth = 50,
+      })(entry, vim_item)
+      local strings = vim.split(kind.kind, "%s", { trimempty = true })
+      kind.kind = " " .. (strings[1] or "") .. " "
+      kind.menu = "    (" .. (strings[2] or "") .. ")"
+
+      return kind
+    end,
+  },
+  mapping = mappings,
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "nvim_lsp_signature_help" },
+    { name = "nvim_lua" },
+    { name = "luasnip" },
+    { name = "emoji" },
+  }, {
+    { name = "buffer" },
+    { name = "path" },
+  }),
+})
+
+cmp.setup.cmdline(":", {
+  mapping = mappings,
+  sources = cmp.config.sources({ { name = "path" } }, {
+    {
+      name = "cmdline",
+      option = {
+        ignore_cmds = { "Man", "!" },
+      },
+    },
+  }),
+})
+
+cmp.setup.cmdline("/", {
+  mapping = mappings,
+  sources = cmp.config.sources({ { name = "buffer" } }),
+})
