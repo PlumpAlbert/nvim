76c093054e450adf18f9a46b7953fcb92ad81889
plump_albert <plumpalbert@gmail.com>
2023-03-06 17:03:29 +0300

    removed tokyonight colorscheme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -26,7 +26,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "vscode", "tokyonight", "habamax" } },
+  install = { colorscheme = { "vscode", "habamax" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
