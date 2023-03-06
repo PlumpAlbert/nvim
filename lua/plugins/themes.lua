361cbab3b26b00e7044dc6f58a14b6e9988b4854
plump_albert <plumpalbert@gmail.com>
2023-03-06 16:02:43 +0300

    added vscode theme

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,23 @@
+return {
+    {
+        'xiyaowong/nvim-transparent',
+        cmd = { "TransparentEnable", "TransparentDisable", "TransparentToggle" },
+        keys = {
+            { '<leader>t', vim.cmd.TransparentToggle, desc = "Toggle transparency" }
+        },
+    },
+    {
+        "Mofiqul/vscode.nvim",
+        config = true,
+        opts = function()
+            local c = require("vscode.colors").get_colors()
+            return {
+                transparent = true,
+                italic_comments = true,
+                group_overrides = {
+                    Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
+                },
+            }
+        end,
+    },
+}
