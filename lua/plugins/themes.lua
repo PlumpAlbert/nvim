f1c0c4e732d5ad56b83142d9c062a2a2dea73c5a
plump_albert <plumpalbert@gmail.com>
2023-03-06 18:43:22 +0300

    added kanagawa theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,23 +1,34 @@
 return {
-    {
-        'xiyaowong/nvim-transparent',
-        cmd = { "TransparentEnable", "TransparentDisable", "TransparentToggle" },
-        keys = {
-            { '<leader>t', vim.cmd.TransparentToggle, desc = "Toggle transparency" }
+  {
+    "xiyaowong/nvim-transparent",
+    cmd = { "TransparentEnable", "TransparentDisable", "TransparentToggle" },
+    keys = {
+      { "<leader>t", vim.cmd.TransparentToggle, desc = "Toggle transparency" },
+    },
+  },
+  {
+    "Mofiqul/vscode.nvim",
+    config = true,
+    opts = function()
+      local c = require("vscode.colors").get_colors()
+      return {
+        transparent = true,
+        italic_comments = true,
+        group_overrides = {
+          Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
         },
+      }
+    end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    opts = {
+      theme = "dragon",
+      background = {
+        dark = "dragon",
+        light = "lotus",
+      },
     },
-    {
-        "Mofiqul/vscode.nvim",
-        config = true,
-        opts = function()
-            local c = require("vscode.colors").get_colors()
-            return {
-                transparent = true,
-                italic_comments = true,
-                group_overrides = {
-                    Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
-                },
-            }
-        end,
-    },
+    config = true,
+  },
 }
