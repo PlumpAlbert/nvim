1747add75b3664b0106bdda85d80369b80f0daa4
plump_albert <plumpalbert@gmail.com>
2023-03-06 18:21:32 +0300

    moved language-specific settings to `plugins/lang` folder

---
+++ lua/plugins/lang/javascript.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "jay-babu/mason-null-ls.nvim",
+    ---@type MasonNullLsSettings
+    opts = {
+      ensure_installed = {
+        "prettierd",
+        "eslint_d",
+      },
+    },
+    config = true,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "emmet-ls",
+        "typescript-language-server",
+        "css-lsp",
+      },
+    },
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local b = require("null-ls.builtins")
+
+      opts.sources = vim.list_extend(opts.sources, {
+        b.diagnostics.eslint_d,
+        b.code_actions.eslint_d,
+        b.formatting.prettierd.with({
+          env = { PRETTIERD_DEFAULT_CONFIG = vim.fn.expand("~/.prettierrc") },
+          extra_filetypes = {
+            "vue",
+            "json",
+            "scss",
+            "markdown",
+          },
+        }),
+      })
+
+      return opts
+    end,
+  },
+}
