7f02cdb10fcfef513a6ded57988bbb5574b570b0
plump_albert <plumpalbert@gmail.com>
2023-03-06 18:21:57 +0300

    added plugin to manage project-local neovim config

--- lua/plugins/ux.lua
+++ lua/plugins/ux.lua
@@ -8,4 +8,18 @@ return {
     },
     cmd = "DogeGenerate",
   },
+  {
+    "klen/nvim-config-local",
+    event = "VimEnter",
+    config = function()
+      require("config-local").setup({
+        config_files = { ".vimrc.lua", ".vimrc" }, -- Config file patterns to load (lua supported)
+        hashfile = vim.fn.stdpath("data") .. "/config-local", -- Where the plugin keeps files data
+        autocommands_create = true, -- Create autocommands (VimEnter, DirectoryChanged)
+        commands_create = true, -- Create commands (ConfigSource, ConfigEdit, ConfigTrust, ConfigIgnore)
+        silent = true, -- Disable plugin messages (Config loaded/ignored)
+        lookup_parents = false, -- Lookup config files in parent directories
+      })
+    end,
+  },
 }
