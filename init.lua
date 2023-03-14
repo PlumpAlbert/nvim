6aaf8393d0a553f8743444ff315eef73a0749a19
plump_albert <plumpalbert@gmail.com>
2023-03-14 11:15:58 +0300

    removed minimal theme

--- init.lua
+++ init.lua
@@ -1,3 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.cmd.colorscheme "minimal"
+vim.cmd.colorscheme "vscode"
--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -31,15 +31,4 @@ return {
     },
     config = true,
   },
-  {
-    "Yazeed1s/minimal.nvim",
-    config = function()
-      vim.g.minimal_italic_comments = true
-      vim.g.minimal_italic_keywords = false
-      vim.g.minimal_italic_booleans = false
-      vim.g.minimal_italic_functions = false
-      vim.g.minimal_italic_variables = false
-      vim.g.minimal_transparent_background = true
-    end,
-  },
 }
