66f371efaf41a583de688d392e52365630de2fbe
plump_albert <plumpalbert@gmail.com>
2023-03-20 09:37:16 +0300

    added nord theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -31,4 +31,16 @@ return {
     },
     config = true,
   },
+  {
+    "shaunsingh/nord.nvim",
+    config = function()
+      -- Example config in lua
+      vim.g.nord_contrast = true
+      vim.g.nord_borders = false
+      vim.g.nord_disable_background = true
+      vim.g.nord_italic = true
+      vim.g.nord_uniform_diff_background = true
+      vim.g.nord_bold = false
+    end,
+  },
 }
