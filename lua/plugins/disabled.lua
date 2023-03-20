e7f581f4ded3f2b109231ce328d7ba57e8b8f2e5
plump_albert <plumpalbert@gmail.com>
2023-03-20 16:49:48 +0300

    disabled some default plugins

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,4 +2,8 @@ return {
   { "ggandor/flit.nvim", enabled = false },
   { "ggandor/leap.nvim", enabled = false },
   { "folke/neoconf.nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
+  { "echasnovski/mini.indentscope", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  { "dstein64/vim-startuptime", enabled = false },
 }
