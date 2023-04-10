86956cd35470bd347959f639fe1ac10c6324c38a
plump_albert <plumpalbert@gmail.com>
2023-04-10 19:25:45 +0300

    added circles.nvim plugin

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -184,4 +184,13 @@ return {
       },
     },
   },
+  {
+    "projekt0n/circles.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      icons = { empty = "", filled = "", lsp_prefix = "" },
+      -- override lsp_diagnostic virtual-text icon with `icons.lsp_prefix`
+      lsp = true,
+    },
+  },
 }
