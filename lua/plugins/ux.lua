3b836ee15e9bf4a8614b7b785c36b03ab7252280
plump_albert <plumpalbert@gmail.com>
2023-04-14 11:11:23 +0300

    added nvim-early-retirement plugin

--- lua/plugins/ux.lua
+++ lua/plugins/ux.lua
@@ -27,4 +27,31 @@ return {
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     config = true,
   },
+  {
+    "chrisgrieser/nvim-early-retirement",
+    opts = {
+      -- if a buffer has been inactive for this many minutes, close it
+      retirementAgeMins = 10,
+      -- filetypes to ignore
+      ignoredFiletypes = {},
+      -- will not close the alternate file
+      ignoreAltFile = true,
+      -- minimum number of open buffers for auto-closing to become active, e.g.,
+      -- by setting this to 4, no auto-closing will take place when you have 3
+      -- or less open buffers. Note that this plugin never closes the currently
+      -- active buffer, so a number < 2 will effectively disable this setting.
+      minimumBufferNum = 3,
+      -- will ignore buffers with unsaved changes. If false, the buffers will
+      -- automatically be written and then closed.
+      ignoreUnsavedChangesBufs = false,
+      -- ignore non-empty buftypes, e.g. terminal buffers
+      ignoreSpecialBuftypes = true,
+      -- ignore visible buffers (buffers open in a window, "a" in `:buffers`)
+      ignoreVisibleBufs = true,
+      -- uses vim.notify for plugins like nvim-notify
+      notificationOnAutoClose = true,
+    },
+    config = true,
+    event = "VeryLazy",
+  },
 }
