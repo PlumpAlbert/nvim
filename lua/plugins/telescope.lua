e89b633914699e7661a734a53ebb8bbe017a9c9c
plump_albert <plumpalbert@gmail.com>
2023-05-17 12:14:15 +0300

    set default theme

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,60 +9,60 @@ return {
 			{
 				"<leader><space>",
 				function()
-					require("telescope.builtin").find_files(
-						require("telescope.themes").get_dropdown({})
-					)
+					require("telescope.builtin").find_files()
 				end,
 				desc = "Find files (Git)",
 			},
 			{
 				"<leader>ff",
 				function()
-					require("telescope.builtin").git_files(
-						require("telescope.themes").get_dropdown({})
-					)
+					require("telescope.builtin").git_files()
 				end,
 				desc = "Find files (CWD)",
 			},
 			{
 				"<leader>/",
 				function()
-					require("telescope.builtin").live_grep(
-						require("telescope.themes").get_dropdown({})
-					)
+					require("telescope.builtin").live_grep()
 				end,
 				desc = "Live grep (project)",
 			},
 			{
-				"<leader>fw",
-				function()
-					require("telescope.builtin").live_grep(
-						require("telescope.themes").get_dropdown({})
-					)
-				end,
-				desc = "Live grep (file)",
-			},
-			{
 				"<leader>fr",
 				function()
-					require("telescope.builtin").resume(
-						require("telescope.themes").get_dropdown({})
-					)
+					require("telescope.builtin").resume()
 				end,
 				desc = "Resume telescope",
 			},
 			{
 				"<leader>,",
 				function()
-					require("telescope.builtin").buffers(
-						require("telescope.themes").get_dropdown({})
-					)
+					require("telescope.builtin").buffers()
+				end,
+				desc = "",
+			},
+			{
+				"<leader>ld",
+				function()
+					require("telescope.builtin").diagnostics({ bufnr = 0 })
 				end,
 				desc = "",
 			},
 		},
-		config = function(_, opts)
-			require("telescope").setup(opts)
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					border = true,
+					sorting_strategy = "ascending",
+					layout_strategy = "vertical",
+					layout_config = {
+						height = 0.7,
+						width = 0.5,
+						preview_height = 0.2,
+						prompt_position = "top",
+					},
+				},
+			})
 			require("telescope").load_extension("lazygit")
 		end,
 	},
