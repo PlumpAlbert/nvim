0cd5455cbf26068cb4842db97bb67acaab3d784f
plump_albert <plumpalbert@gmail.com>
2023-05-17 10:52:32 +0300

    replace eslint_d with eslint

--- lua/config/nullls.lua
+++ lua/config/nullls.lua
@@ -2,7 +2,8 @@ local ns = require("null-ls")
 
 ns.setup({
 	sources = {
-		ns.builtins.diagnostics.eslint_d,
+		ns.builtins.diagnostics.eslint,
+		ns.builtins.code_actions.eslint,
 		ns.builtins.formatting.stylua,
 		ns.builtins.formatting.prettier,
 	},
