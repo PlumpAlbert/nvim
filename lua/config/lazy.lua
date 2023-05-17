3af501678c0e74ab6a8cb125a586b91b430bef22
plump_albert <plumpalbert@gmail.com>
2023-05-17 12:14:48 +0300

    do not notify on updates

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -104,13 +104,13 @@ require("lazy").setup({
 		-- automatically check for plugin updates
 		enabled = false,
 		concurrency = nil, ---@type number? set to 1 to check for updates very slowly
-		notify = true, -- get a notification when new updates are found
+		notify = false, -- get a notification when new updates are found
 		frequency = 3600, -- check for updates every hour
 	},
 	change_detection = {
 		-- automatically check for config file changes and reload the ui
 		enabled = true,
-		notify = true, -- get a notification when changes are found
+		notify = false, -- get a notification when changes are found
 	},
 	performance = {
 		cache = {
@@ -120,7 +120,7 @@ require("lazy").setup({
 		rtp = {
 			reset = true, -- reset the runtime path to $VIMRUNTIME and your config directory
 			---@type string[]
-			paths = {}, -- add any custom paths here that you want to includes in the rtp
+			paths = {},  -- add any custom paths here that you want to includes in the rtp
 			---@type string[] list any plugins you want to disable here
 			disabled_plugins = {
 				"gzip",
