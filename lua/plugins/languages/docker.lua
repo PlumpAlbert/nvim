84a4bb4a63b2d97ec6d19c87cd8d1bceff7adee7
plump_albert <plumpalbert@gmail.com>
2023-05-26 11:32:10 +0300

    updated docker config

--- lua/plugins/languages/docker.lua
+++ lua/plugins/languages/docker.lua
@@ -5,7 +5,26 @@ return {
 			return {
 				ensure_installed = vim.list_extend(
 					opts.ensure_installed,
-					{ "dockerls", "yamlls" }
+					{ "dockerls", "docker_compose_language_service" }
+				),
+				handlers = vim.tbl_extend("force", opts.setup_handlers or {}, {
+					docker_compose_language_service = function()
+						require("lspconfig").docker_compose_language_service.setup({
+							capabilities = require("config.lsp").capabilities,
+							filetypes = { "yaml.docker" },
+						})
+					end,
+				}),
+			}
+		end,
+	},
+	{
+		"jay-babu/mason-null-ls.nvim",
+		opts = function(_, opts)
+			return {
+				ensure_installed = vim.list_extend(
+					opts.ensure_installed or {},
+					{ "yamllint" }
 				),
 			}
 		end,
