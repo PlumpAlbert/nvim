6cad317b0b736f751c77fdb28937d980a6ae4cd0
plump_albert <plumpalbert@gmail.com>
2023-05-26 11:31:51 +0300

    enable autoformat for prettier

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,1 +1,14 @@
 require("config.lazy")
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = { "*.js", "*.ts", "*.jsx", "*.tsx", "*.css", "*.scss", "*.sass" },
+	callback = function()
+		vim.lsp.buf.format({
+			async = true,
+			bufnr = 0,
+			filter = function(c)
+				return c.name ~= "tsserver"
+			end,
+		})
+	end,
+})
