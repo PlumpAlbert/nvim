b1f2029eb8e12c44ac0b55d63ff396a75e4460af
Матвей Федин <plumpalbert@gmail.com>
2023-06-30 13:05:01 +0300

    neotree: trash instead of delete

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -79,6 +79,59 @@ local plugins = {
 				},
 			},
 			filesystem = {
+				commands = {
+					delete = function(state)
+						local inputs = require("neo-tree.ui.inputs")
+						local path = state.tree:get_node().path
+
+						inputs.confirm(
+							"Wanna trash " .. path,
+							function(confirmed)
+								if not confirmed then
+									return
+								end
+								vim.fn.system({
+									"trash",
+									vim.fn.fnameescape(path),
+								})
+								require("neo-tree.sources.manager").refresh(
+									state.name
+								)
+							end
+						)
+					end,
+					delete_visual = function(state, selected_nodes)
+						local inputs = require("neo-tree.ui.inputs")
+
+						-- get table items count
+						function GetTableLen(tbl)
+							local len = 0
+							for n in pairs(tbl) do
+								len = len + 1
+							end
+							return len
+						end
+
+						local count = GetTableLen(selected_nodes)
+						local msg = "Are you sure you want to trash "
+							.. count
+							.. " files?"
+						inputs.confirm(msg, function(confirmed)
+							if not confirmed then
+								return
+							end
+							for _, node in ipairs(selected_nodes) do
+								vim.fn.system({
+									"trash",
+									vim.fn.fnameescape(node.path),
+								})
+							end
+							require("neo-tree.sources.manager").refresh(
+								state.name
+							)
+						end)
+					end,
+				},
 				filtered_items = {
 					hide_dotfiles = true,
 					hide_gitignored = true,
