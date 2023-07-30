158b2f9f5d86d449f7b81991a5165fceae6d7b61
Матвей Федин <plumpalbert@gmail.com>
2023-07-30 15:59:09 +0300

    setting notification background to transparent

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -1,3 +1,9 @@
 return {
     { "wakatime/vim-wakatime", lazy = false },
+    {
+        "rcarriga/nvim-notify",
+        opts = {
+            background_colour = "#00000000",
+        },
+    },
 }
