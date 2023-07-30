417b1d5ddc84566b7c92d077e9becf35a24ccffd
Матвей Федин <plumpalbert@gmail.com>
2023-07-30 13:47:12 +0300

    fixed bug with tsx highlighting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -18,7 +18,19 @@ return {
         ensure_installed = { "lua", "vim", "bash" },
         auto_install = true,
         indent = { enable = true },
-        highlight = { enable = true },
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+          disable = function(lang, buf)
+            if vim.tbl_contains({ "latex" }, lang) then
+              return true
+            end
+
+            local status_ok, big_file_detected =
+              pcall(vim.api.nvim_buf_get_var, buf, "bigfile_disable_treesitter")
+            return status_ok and big_file_detected
+          end,
+        },
         autotag = { enable = true },
         rainbow = {
           enable = true,
