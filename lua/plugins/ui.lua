202fbb4b4320d1f91b1cd80d7db2bfd060ccf005
Матвей Федин <plumpalbert@gmail.com>
2023-08-01 10:18:59 +0300

    added window-picker plugin

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -7,4 +7,40 @@ return {
             space_char_blankline = " ",
         },
     },
+
+    {
+        "s1n7ax/nvim-window-picker",
+        version = "2.*",
+        event = "VeryLazy",
+        opts = {
+            hint = "floating-big-letter",
+
+            highlights = {
+                statusline = {
+                    focused = {
+                        fg = "#ededed",
+                        bg = "#e35e4f",
+                        bold = true,
+                    },
+                    unfocused = {
+                        fg = "#ededed",
+                        bg = "#44cc41",
+                        bold = true,
+                    },
+                },
+                winbar = {
+                    focused = {
+                        fg = "#ededed",
+                        bg = "#e35e4f",
+                        bold = true,
+                    },
+                    unfocused = {
+                        fg = "#ededed",
+                        bg = "#44cc41",
+                        bold = true,
+                    },
+                },
+            },
+        },
+    },
 }
