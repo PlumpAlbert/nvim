74b173caed0914e9568d1e72e6da8e16bddccc0d
Матвей Федин <plumpalbert@gmail.com>
2023-08-01 10:24:06 +0300

    open neo-tree in float window

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -1,9 +1,19 @@
 return {
     { "wakatime/vim-wakatime", lazy = false },
+
     {
         "rcarriga/nvim-notify",
         opts = {
             background_colour = "#00000000",
+        },
+    },
+
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        opts = {
+            window = {
+                position = "float",
+            },
         },
     },
 }
