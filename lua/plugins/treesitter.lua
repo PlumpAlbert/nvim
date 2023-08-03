465c4fd3a38f79361c5e22ecb5b3ba89658225ad
Матвей Федин <plumpalbert@gmail.com>
2023-08-03 18:50:15 +0300

    remove rainbows since it's broken in TSX

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,6 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		dependencies = {
-			"HiPhish/nvim-ts-rainbow2",
 			"windwp/nvim-ts-autotag",
 			"JoosepAlviste/nvim-ts-context-commentstring",
 		},
@@ -21,13 +20,6 @@ return {
 				indent = { enable = true },
 				autotag = { enable = true },
 				context_commentstring = { enable = true },
-				rainbow = {
-					enable = true,
-					-- Which query to use for finding delimiters
-					query = "rainbow-parens",
-					-- Highlight the entire buffer all at once
-					strategy = require("ts-rainbow").strategy.global,
-				},
 			})
 		end,
 	},
