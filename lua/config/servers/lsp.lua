4f8d974946458820e0980aa818a9908cfacef791
Матвей Федин <plumpalbert@gmail.com>
2023-08-04 15:44:00 +0300

    unset keybindings on LspDetach

--- lua/config/servers/lsp.lua
+++ lua/config/servers/lsp.lua
@@ -42,4 +42,152 @@ M.options = {
 	},
 }
 
+---@class LspKeymap
+---@field mode ('v'|'n'|'x'|'s')[]|nil
+---@field key string
+---@field action string|function
+---@field desc string
+
+---@type LspKeymap[]
+M.keys = {
+	--#region diagnostics
+	{
+		key = "[d",
+		action = vim.diagnostic.goto_prev,
+		desc = "Go to previous diagnostic",
+	},
+	{
+		key = "]d",
+		action = vim.diagnostic.goto_next,
+		desc = "Go to next diagnostic",
+	},
+	{
+		key = "<leader>d",
+		action = vim.diagnostic.open_float,
+		desc = "Show line diagnostics",
+	},
+	--#endregion
+
+	--#region folds
+	{
+		key = "zR",
+		action = function()
+			require("ufo").openAllFolds()
+		end,
+		desc = "Open all folds",
+	},
+	{
+		key = "zr",
+		action = function()
+			local kinds = { "imports", "comment" }
+			require("ufo").openFoldsExceptKinds(kinds)
+		end,
+		desc = "Open folds, except comments and imports",
+	},
+	{
+		key = "zM",
+		action = function()
+			require("ufo").closeAllFolds()
+		end,
+		desc = "Close all folds",
+	},
+	{
+		key = "zm",
+		action = function()
+			require("ufo").closeFoldsWith(0)
+		end,
+		desc = "Close folds with",
+	},
+	--#endregion
+
+	--#region goto
+	{
+		key = "gD",
+		action = vim.lsp.buf.declaration,
+		desc = "Go to declaration",
+	},
+	{
+		key = "gD",
+		action = vim.lsp.buf.definition,
+		desc = "Go to definition",
+	},
+	{
+		key = "gi",
+		action = vim.lsp.buf.implementation,
+		desc = "Go to implementation",
+	},
+	{
+		key = "gr",
+		action = vim.lsp.buf.references,
+		desc = "Go to references",
+	},
+	{
+		key = "gT",
+		action = vim.lsp.buf.type_definition,
+		desc = "Go to type definition",
+	},
+	--#endregion
+
+	--#region workspaces
+	{
+		key = "<leader>wa",
+		action = vim.lsp.buf.add_workspace_folder,
+		desc = "Add workspace folder",
+	},
+	{
+		key = "<leader>wr",
+		action = vim.lsp.buf.add_workspace_folder,
+		desc = "Remove workspace folder",
+	},
+	{
+		key = "<leader>wl",
+		action = function()
+			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		end,
+		desc = "Print workspace folders",
+	},
+	--#endregion
+
+	--#region misc
+	{
+		key = "K",
+		action = function()
+			local winid = require("ufo").peekFoldedLinesUnderCursor()
+			if not winid then
+				vim.lsp.buf.hover()
+			end
+		end,
+		desc = "Symbol info",
+	},
+	{
+		mode = { "n", "i" },
+		key = "<C-p>",
+		action = vim.lsp.buf.signature_help,
+		desc = "Signature help",
+	},
+	{
+		key = "<leader>lr",
+		action = vim.lsp.buf.rename,
+		desc = "Rename symbol",
+	},
+	{
+		key = "<leader>la",
+		action = vim.lsp.buf.code_action,
+		desc = "Code actions",
+	},
+	{
+		key = "<leader>lf",
+		action = function()
+			vim.lsp.buf.format({
+				async = true,
+				filter = function(client)
+					return client.name ~= "tsserver"
+				end,
+			})
+		end,
+		desc = "Fomat document",
+	},
+	--#endregion
+}
+
 return M
