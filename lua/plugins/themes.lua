ecb9b00b6ee19d215691d6d550ed394d650beb79
Матвей Федин <plumpalbert@gmail.com>
2023-08-10 18:47:10 +0300

    modify catppuccin cmp integration

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -3,29 +3,75 @@ return {
 		"catppuccin/nvim",
 		priority = 9999,
 		name = "catppuccin",
-		opts = {
-			transparent_background = false,
-			term_colors = true,
-			compile = {
-				enabled = true,
-				path = vim.fn.stdpath("cache") .. "/catppuccin",
-			},
-			styles = {
-				comments = { "italic" },
-				conditionals = { "italic" },
-			},
-			integrations = {
-				gitsigns = true,
-				mason = true,
-				mini = true,
-				neotree = true,
-				cmp = true,
-				treesitter = true,
-				ts_rainbow2 = true,
-				telescope = true,
-				which_key = true,
-			},
-		},
+		opts = function()
+			return {
+				transparent_background = false,
+				term_colors = true,
+				compile = {
+					enabled = true,
+					path = vim.fn.stdpath("cache") .. "/catppuccin",
+				},
+				styles = {
+					comments = { "italic" },
+					conditionals = { "italic" },
+				},
+				integrations = {
+					gitsigns = true,
+					mason = true,
+					mini = true,
+					neotree = true,
+					cmp = true,
+					treesitter = true,
+					ts_rainbow2 = true,
+					telescope = true,
+					which_key = true,
+				},
+				custom_highlights = function(C)
+					return {
+						CmpItemAbbr = { fg = C.overlay2 },
+						CmpItemAbbrDeprecated = {
+							fg = C.overlay0,
+							style = { "strikethrough" },
+						},
+						CmpItemKind = { fg = C.blue },
+						CmpItemMenu = { fg = C.text, bg = C.overlay4 },
+						CmpItemAbbrMatch = { fg = C.text, style = { "bold" } },
+						CmpItemAbbrMatchFuzzy = {
+							fg = C.text,
+							style = { "bold" },
+						},
+
+						-- kind support
+						CmpItemKindSnippet = { bg = C.mauve, fg = C.base },
+						CmpItemKindKeyword = { bg = C.red, fg = C.base },
+						CmpItemKindText = { bg = C.teal, fg = C.base },
+						CmpItemKindMethod = { bg = C.blue, fg = C.base },
+						CmpItemKindConstructor = { bg = C.blue, fg = C.base },
+						CmpItemKindFunction = { bg = C.blue, fg = C.base },
+						CmpItemKindFolder = { bg = C.blue, fg = C.base },
+						CmpItemKindModule = { bg = C.blue, fg = C.base },
+						CmpItemKindConstant = { bg = C.peach, fg = C.base },
+						CmpItemKindField = { bg = C.green, fg = C.base },
+						CmpItemKindProperty = { bg = C.green, fg = C.base },
+						CmpItemKindEnum = { bg = C.green, fg = C.base },
+						CmpItemKindUnit = { bg = C.green, fg = C.base },
+						CmpItemKindClass = { bg = C.yellow, fg = C.base },
+						CmpItemKindVariable = { bg = C.flamingo, fg = C.base },
+						CmpItemKindFile = { bg = C.blue, fg = C.base },
+						CmpItemKindInterface = { bg = C.yellow, fg = C.base },
+						CmpItemKindColor = { bg = C.red, fg = C.base },
+						CmpItemKindReference = { bg = C.red, fg = C.base },
+						CmpItemKindEnumMember = { bg = C.red, fg = C.base },
+						CmpItemKindStruct = { bg = C.blue, fg = C.base },
+						CmpItemKindValue = { bg = C.peach, fg = C.base },
+						CmpItemKindEvent = { bg = C.blue, fg = C.base },
+						CmpItemKindOperator = { bg = C.blue, fg = C.base },
+						CmpItemKindTypeParameter = { bg = C.blue, fg = C.base },
+						CmpItemKindCopilot = { bg = C.teal, fg = C.base },
+					}
+				end,
+			}
+		end,
 		config = true,
 	},
 	{
