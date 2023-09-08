370ad93a94435bbb72656972984dd5c8f961c7aa
Matvey Fedin <matvey.fedin@axenix.pro>
2023-09-08 11:17:40 +0300

    enabled Trouble.nvim

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,6 +1,6 @@
 return {
     -- disable trouble
-    { "folke/trouble.nvim", enabled = false },
+    -- { "folke/trouble.nvim", enabled = false },
     { "folke/flash.nvim", enabled = false },
     { "folke/todo-comments.nvim", enabled = false },
     { "echasnovski/mini.indentscope", enabled = false },
