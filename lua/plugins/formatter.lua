b1f6102a7059ae5c305d642eb4a088519211a7a8
plump_albert <plumpalbert@gmail.com>
2023-09-20 15:29:08 +0300

    added formatter

---
+++ lua/plugins/formatter.lua
@@ -0,0 +1,45 @@
+local M = {
+	"mhartington/formatter.nvim",
+	cmd = {
+		"Format",
+		"FormatWrite",
+		"FormatLock",
+		"FormatWriteLock",
+	},
+}
+
+function M.opts()
+	local js = {
+		require("formatter.defaults.prettierd"),
+	}
+
+	return {
+		logging = true,
+		log_level = vim.log.levels.WARN,
+		filetype = {
+			lua = { require("formatter.filetypes.lua").stylua },
+			javascript = js,
+			javascriptreact = js,
+			typescript = js,
+			typescriptreact = js,
+			css = js,
+			scss = js,
+			sass = js,
+			html = js,
+			markdown = js,
+			json = js,
+		},
+	}
+end
+
+M.keys = {
+	{ "<leader>lf", "<cmd>Format<cr>", desc = "Format buffer" },
+	{ "<leader>lF", "<cmd>FormatWrite<cr>", desc = "Format buffer (write)" },
+}
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+	group = vim.api.nvim_create_augroup("FormatAutogroup", {}),
+	command = "FormatWriteLock",
+})
+
+return M
