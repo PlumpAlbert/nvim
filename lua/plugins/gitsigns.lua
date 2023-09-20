773fe6533593b1e3094e86e0b6878e722fcff683
plump_albert <plumpalbert@gmail.com>
2023-09-20 16:22:31 +0300

    added gitsigns

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,29 @@
+local M = {
+	"lewis6991/gitsigns.nvim",
+	config = true,
+	cmd = { "Gitsigns" },
+	opts = {
+		signcolumn = false,
+		numhl = true,
+		linehl = false,
+		word_diff = false,
+		attach_to_untracked = true,
+		current_line_blame = false,
+	},
+	keys = {
+		{ "<leader>ghp", "<cmd>Gitsigns preview_hunk<cr>", desc = "Preview git hunk" },
+		{ "<leader>ghs", "<cmd>Gitsigns stage_hunk<cr>", mode = { "v", "n" }, desc = "Stage hunk" },
+		{
+			"<leader>gb",
+			function()
+				require("gitsigns").blame_line({ full = true })
+			end,
+			mode = { "v", "n" },
+			desc = "Stage hunk",
+		},
+		{ "]c", "<cmd>Gitsigns next_hunk" },
+		{ "[c", "<cmd>Gitsigns prev_hunk" },
+	},
+}
+
+return M
