0da1074534884f5def8311cf3d3a7308b8059f70
plump_albert <plumpalbert@gmail.com>
2023-09-20 15:08:25 +0300

    added keymaps for nvim-ufo

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,7 +3,16 @@ local M = {
 	event = "BufReadPre",
 	dependencies = {
 		"hrsh7th/cmp-nvim-lsp",
-		{"kevinhwang91/nvim-ufo", dependencies = {"kevinhwang91/promise-async"}}
+		{
+			"kevinhwang91/nvim-ufo",
+			dependencies = {"kevinhwang91/promise-async"},
+			keys = {
+				{ "zM", function() require"ufo".closeAllFolds() end },
+				{ "zR", function() require"ufo".openAllFolds() end },
+				{ "zm", function() require"ufo".closeFoldsWith(-1) end },
+				{ "zr", function() require"ufo".closeFoldsWith(1) end },
+			}
+		}
 	}
 }
 
