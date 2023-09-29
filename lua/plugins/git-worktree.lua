e142eb7f8352c8bb6a851920d75bc8b000414243
Plump Albert <plumpalbert@gmail.com>
2023-09-29 23:07:52 +0300

    added plugin to work with git worktrees

---
+++ lua/plugins/git-worktree.lua
@@ -0,0 +1,19 @@
+local M = {
+	"ThePrimeagen/git-worktree.nvim",
+	dependencies = { "nvim-telescope/telescope.nvim" },
+	name = "git-worktree",
+	config = function()
+		require("telescope").load_extension("git_worktree")
+
+		require("git-worktree").setup({
+			update_on_change = true,
+			clearjumps_on_change = true,
+		})
+	end,
+	keys = {
+		{ "<leader>gwl", "<cmd>Telescope git_worktree git_worktrees<cr>", desc = "List worktrees" },
+		{ "<leader>gwa", "<cmd>Telescope git_worktree create_git_worktree<cr>", desc = "Create new worktree" },
+	},
+}
+
+return M
