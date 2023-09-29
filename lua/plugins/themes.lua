0d2fcd2075c58b6a6d0d6b95bbb50559e6346b20
plump_albert <plumpalbert@gmail.com>
2023-09-29 21:13:07 +0300

    added themes

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,6 +1,8 @@
 return {
-	{ 'catppuccin/nvim',
+	{
+		"catppuccin/nvim",
 		name = "catppuccin",
+		config = true,
 		opts = {
 			transparent_background = true,
 			term_colors = true,
@@ -13,6 +15,46 @@ return {
 				conditionals = { "italic" },
 			},
 		},
-		config = true
-	}
+	},
+	{
+		"folke/tokyonight.nvim",
+		name = "tokyonight",
+		lazy = false,
+		priority = 1000,
+		config = true,
+		opts = {
+			style = "night",
+			light_style = "day",
+			transparent = true,
+			terminal_colors = true,
+			sidebars = { "qf", "help", "lazy", "neo-tree" },
+			hide_inactive_statusline = true,
+		},
+	},
+	{
+		"projekt0n/github-nvim-theme",
+		name = "github-theme",
+		lazy = false,
+		priority = 1000,
+		config = true,
+		opts = {
+			options = {
+				compile_path = vim.fn.stdpath("cache") .. "/github-theme",
+				compile_file_suffix = "_compiled",
+				hide_end_of_buffer = true,
+				transparent = true,
+				terminal_colors = true,
+				module_default = true,
+				styles = {
+					comments = "italic",
+					keywords = "italic,bold",
+					strings = "NONE",
+				},
+				inverse = {
+					search = true,
+					visual = false,
+				},
+			},
+		},
+	},
 }
