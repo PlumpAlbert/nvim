427a0d005d0a45b9e7dc180400a9eb14a179255b
Plump Albert <plumpalbert@gmail.com>
2023-10-02 12:49:20 +0300

    added fidget.nvim plugin

---
+++ lua/plugins/fidget.lua
@@ -0,0 +1,31 @@
+local M = {
+	"j-hui/fidget.nvim",
+	tag = "legacy",
+	event = "LspAttach",
+	config = true,
+	opts = {
+		align = {
+			bottom = false,
+			right = true,
+		},
+		text = {
+			spinner = "clock",
+			done = "✔ ",
+			commenced = " ",
+			completed = " ",
+		},
+		timer = {
+			spinner_rate = 100,
+			fidget_decay = 1000,
+			task_decay = 500,
+		},
+		window = {
+			relative = "editor",
+			blend = 0,
+			zindex = 1,
+			border = "none",
+		},
+	},
+}
+
+return M
