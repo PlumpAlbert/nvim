037d7f12205b9e89deb144c9fbe9aa31ee848d6c
Plump Albert <plumpalbert@gmail.com>
2023-10-19 19:26:13 +0300

    added neovide config

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
 require("options")
 require("config/lazy")
 require("colorscheme")
+
+if vim.g.neovide then
+	require("config.neovide")
+end
---
+++ lua/config/neovide.lua
@@ -0,0 +1,2 @@
+vim.o.guifont = "Iosevka_Fixed,Cascadia_Code,Consolas,monospaced,Nerd_Font_Symbols:h10"
+vim.g.neovide_remember_window_size = false
