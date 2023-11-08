ec3cbc919870d8a2515975581e91cedaceadc56a
Plump Albert <plumpalbert@gmail.com>
2023-11-08 21:53:34 +0300

    fixed bug with capabilities

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -53,13 +53,12 @@ M.opts = function()
 	local lspconfig = require("lspconfig")
 	local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-	local capabilities = vim.lsp.protocol.make_client_capabilities()
+	local capabilities = cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
 	capabilities.textDocument.completion.completionItem.snippetSupport = true
 	capabilities.textDocument.foldingRange = {
 		dynamicRegistration = false,
 		lineFoldingOnly = true,
 	}
-	capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 	require("ufo").setup()
 
 	return {
