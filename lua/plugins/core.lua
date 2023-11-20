2fd8c292b79dcb170756b9c66ec23576831d2f87
Plump Albert <plumpalbert@gmail.com>
2023-11-20 10:19:39 +0300

    auto configure neoconf and neodev

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,7 +1,7 @@
 -- vim:ft=lua:ts=4:sw=0:noet
 return {
 	{ "folke/which-key.nvim", config = true },
-	{ "folke/neoconf.nvim", cmd = "Neoconf", config = function() end },
-	{ "folke/neodev.nvim", config = function() end },
+	{ "folke/neoconf.nvim", cmd = "Neoconf", config = true },
+	{ "folke/neodev.nvim", config = true },
 	{ "wakatime/vim-wakatime", event = "VeryLazy", config = function() end },
 }
