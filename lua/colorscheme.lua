ef916c6e54d3a3413896ae6d93a05cc1066d5d81
Plump Albert <plumpalbert@gmail.com>
2023-11-23 10:38:58 +0300

    truly random scheme each time

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,14 +1,15 @@
 vim.g.background = "dark"
 local schemes = {
 	"vscode",
-	"catppuccin",
 	"github_dark",
+	"catppuccin",
 }
 
+math.randomseed(os.time())
 local function set_colorscheme()
 	local scheme = schemes[math.random(#schemes)]
 	vim.cmd.colorscheme(scheme)
 end
 
 set_colorscheme()
-vim.keymap.set("n", "<leader><F5>", set_colorscheme, { desc = "Random colorscheme" })
+vim.keymap.set("n", "<F5>", set_colorscheme, { desc = "Random colorscheme" })
