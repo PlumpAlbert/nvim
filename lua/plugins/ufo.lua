a9c5b7649834ee9441cb0e30a9bd84960ca07ee0
Plump Albert <plumpalbert@gmail.com>
2023-11-23 19:57:29 +0300

    updated ufo

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -1,8 +1,41 @@
-return {
+local M = {
 	"kevinhwang91/nvim-ufo",
 	enabled = false,
-	name = "ufo",
-	dependencies = { "kevinhwang91/promise-async" },
+	event = "VeryLazy",
+	dependencies = {
+		"kevinhwang91/promise-async",
+		{
+			"luukvbaal/statuscol.nvim",
+			config = function()
+				local builtin = require("statuscol.builtin")
+				require("statuscol").setup({
+					relculright = true,
+					segments = {
+						{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+						{ text = { "%s" }, click = "v:lua.ScSa" },
+						{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+					},
+				})
+			end,
+		},
+	},
+	config = function()
+		vim.opt.fillchars = {
+			eob = " ",
+			fold = " ",
+			foldopen = "",
+			foldsep = " ",
+			foldclose = "",
+		}
+		vim.o.foldcolumn = "1" -- '0' is not bad
+		vim.o.foldlevel = 9999 -- Using ufo provider need a large value, feel free to decrease the value
+		vim.o.foldlevelstart = 9999
+		vim.o.foldenable = true
+
+		require("ufo").setup({
+			close_fold_kinds = { "imports", "comment" },
+		})
+	end,
 	keys = {
 		{
 			"zM",
@@ -30,3 +63,5 @@ return {
 		},
 	},
 }
+
+return M
