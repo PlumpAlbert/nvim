921335630056a64761499de1ac4927c937e069f2
Plump Albert <plumpalbert@gmail.com>
2024-03-01 18:06:27 +0300

    passing down handlers

--- lua/neowim/plugins/mason-lspconfig.lua
+++ lua/neowim/plugins/mason-lspconfig.lua
@@ -6,12 +6,13 @@ return {
 			"lua_ls",
 		}, opts.ensure_installed or {})
 
-		local handlers = vim.tbl_extend("force", {
+		local handlers = vim.tbl_extend("force", opts.handlers or {}, {
 			require("lsp-zero").default_setup,
-		}, opts.handlers or {})
+		})
 
 		require("mason-lspconfig").setup({
 			ensure_installed = ensure_installed,
+			handlers = handlers,
 		})
 	end,
 }
