2b17b7d420a33f5edecff6501ed7884980e0d7fa
Plump Albert <plumpalbert@gmail.com>
2024-03-01 17:07:15 +0300

    install lazy

---
+++ init.lua
@@ -0,0 +1,45 @@
+require('neowim/options')
+require('neowim/vscode-options')
+
+-- #region package manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	spec = {
+		{ import = "neowim/plugins" },
+		-- { "plumpalbert/nvim-typescript", import = "plump-typescript" },
+	},
+	lockfile = vim.fn.stdpath("data") .. "/lazy/lazy-lock.json",
+	install = { colorscheme = { "onedark", "habamax", "default" } },
+	checker = { enabled = true, notify = false }, -- automatically check for plugin updates
+	change_detection = { enabled = true, notify = false }, -- automatically check for configuration update
+	performance = {
+		rtp = {
+			-- disable some rtp plugins
+			disabled_plugins = {
+				"gzip",
+				-- "matchit",
+				-- "matchparen",
+				-- "netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
+})
+-- #endregion
