05ff188b3138f745932b93a6e15f642ab3acc6e2
Plump Albert <plumpalbert@gmail.com>
2024-03-01 17:34:26 +0300

    mapping leader to space

--- lua/neowim/options.lua
+++ lua/neowim/options.lua
@@ -1,3 +1,6 @@
-vim.opt.langmap = "ёйцукенгшщзхъфывапролджэячсмитьбюЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ;`qwertyuiop[]asdfghjkl\\;'zxcvbnm\\,.~QWERTYUIOP{}ASDFGHJKL:\\\"ZXCVBNM<>"
+vim.g.mapleader = " "
+
+vim.opt.langmap =
+	"ёйцукенгшщзхъфывапролджэячсмитьбюЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ;`qwertyuiop[]asdfghjkl\\;'zxcvbnm\\,.~QWERTYUIOP{}ASDFGHJKL:\\\"ZXCVBNM<>"
 vim.opt.number = true
 vim.opt.relativenumber = true
