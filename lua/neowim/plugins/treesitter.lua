b9295a65fcceadd2aff590ca863a1bf7603c10e3
plump_albert <plumpalbert@gmail.com>
2024-03-04 12:12:49 +0300

    using different plugin for rainbow parenthesis

--- lua/neowim/plugins/treesitter.lua
+++ lua/neowim/plugins/treesitter.lua
@@ -1,83 +1,116 @@
-return {
+local M = {
 	"nvim-treesitter/nvim-treesitter",
-	dependencies = {
-		{
-			"nvim-treesitter/nvim-treesitter-textobjects",
-			dependencies = "nvim-treesitter/nvim-treesitter",
-		},
-		{
-			"HiPhish/nvim-ts-rainbow2",
-			dependencies = "nvim-treesitter/nvim-treesitter",
-		},
-		{
-			"windwp/nvim-ts-autotag",
-			dependencies = "nvim-treesitter/nvim-treesitter",
-		},
-		{
-			"JoosepAlviste/nvim-ts-context-commentstring",
-			dependencies = "nvim-treesitter/nvim-treesitter",
-		},
-	},
 	build = ":TSUpdate",
-	config = function(_, opts)
-		opts = opts or {}
+}
 
-		require("nvim-treesitter.install").prefer_git = false
+local rainbow = {
+	"HiPhish/rainbow-delimiters.nvim",
+	dependencies = "nvim-treesitter/nvim-treesitter",
+	config = function()
+		local rainbow_delimiters = require("rainbow-delimiters")
 
-		local ensure_installed = vim.list_extend({
-			"lua",
-			"vim",
-			"vimdoc",
-			"markdown",
-			"markdown_inline",
-			"comment",
-		}, opts.ensure_installed or {})
-
-		require("nvim-treesitter.configs").setup({
-			auto_install = false,
-			ensure_installed = ensure_installed,
-			highlight = { enable = true },
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "gis",
-					node_incremental = "]sn",
-					scope_incremental = "]ss",
-					node_decremental = "<BS>",
-				},
+		require("rainbow-delimiters.setup").setup({
+			strategy = {
+				[""] = rainbow_delimiters.strategy["global"],
+				vim = rainbow_delimiters.strategy["local"],
 			},
-			textobjects = {
-				select = {
-					enable = true,
-					lookagead = true,
-					keymaps = {
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-					},
-				},
+			query = {
+				[""] = "rainbow-delimiters",
+				lua = "rainbow-blocks",
 			},
-			indent = { enable = true },
-			rainbow = {
-				enable = true,
-				query = {
-					"rainbow-parens",
-					html = "rainbow-tags",
-					tsx = "rainbow-tags",
-					vue = "rainbow-tags",
-					javascript = "rainbow-parens-react",
-					latex = "rainbow-blocks",
-				},
+			priority = {
+				[""] = 110,
+				lua = 210,
 			},
-			autotag = {
-				enable = true,
-				enable_rename = true,
-				enable_close = true,
-				enable_close_on_slash = false,
-			},
-			ts_context_commentstring = {
-				enable = true,
-				enable_autocmd = false,
+			highlight = {
+				"RainbowDelimiterRed",
+				"RainbowDelimiterYellow",
+				"RainbowDelimiterBlue",
+				"RainbowDelimiterOrange",
+				"RainbowDelimiterGreen",
+				"RainbowDelimiterViolet",
+				"RainbowDelimiterCyan",
 			},
 		})
 	end,
 }
+
+M.dependencies = {
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+	},
+	rainbow,
+	{
+		"windwp/nvim-ts-autotag",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+	},
+	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+	},
+}
+
+M.config = function(_, opts)
+	opts = opts or {}
+
+	require("nvim-treesitter.install").prefer_git = false
+
+	local ensure_installed = vim.list_extend({
+		"lua",
+		"vim",
+		"vimdoc",
+		"markdown",
+		"markdown_inline",
+		"comment",
+	}, opts.ensure_installed or {})
+
+	require("nvim-treesitter.configs").setup({
+		auto_install = false,
+		ensure_installed = ensure_installed,
+		highlight = { enable = true },
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "gis",
+				node_incremental = "]sn",
+				scope_incremental = "]ss",
+				node_decremental = "<BS>",
+			},
+		},
+		textobjects = {
+			select = {
+				enable = true,
+				lookagead = true,
+				keymaps = {
+					["af"] = "@function.outer",
+					["if"] = "@function.inner",
+				},
+			},
+		},
+		indent = { enable = true },
+		-- rainbow = {
+		-- 	enable = true,
+		-- 	query = {
+		-- 		"rainbow-parens",
+		-- 		html = "rainbow-tags",
+		-- 		tsx = "rainbow-tags",
+		-- 		vue = "rainbow-tags",
+		-- 		javascript = "rainbow-parens-react",
+		-- 		latex = "rainbow-blocks",
+		-- 	},
+		-- },
+		autotag = {
+			enable = true,
+			enable_rename = true,
+			enable_close = true,
+			enable_close_on_slash = false,
+		},
+		ts_context_commentstring = {
+			enable = true,
+			enable_autocmd = false,
+		},
+	})
+end
+
+return M
