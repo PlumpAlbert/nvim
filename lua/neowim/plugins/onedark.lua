04a1365f826b481ce64c3e20c6924a559c8134bd
Plump Albert <plumpalbert@gmail.com>
2024-03-04 16:27:23 +0300

    added onedark

---
+++ lua/neowim/plugins/onedark.lua
@@ -0,0 +1,29 @@
+return {
+	'navarasu/onedark.nvim',
+	priority = 1000,
+	config = function()
+		require('onedark').setup({
+			style = 'warmer',
+			transparent = true,
+			ending_tildes = false,
+			term_colors = true,
+			cmp_itemkind_reverse = true,
+			code_style = {
+				comments = 'italic',
+				keywords = 'none',
+				functions = 'none',
+				strings = 'none',
+				variables = 'none'
+			},
+			diagnostics = {
+				darker = true, -- darker colors for diagnostic
+				undercurl = true, -- use undercurl instead of underline for diagnostics
+				background = true, -- use background color for virtual text
+			},
+
+		})
+
+		vim.cmd('colorscheme onedark')
+	end,
+}
+
