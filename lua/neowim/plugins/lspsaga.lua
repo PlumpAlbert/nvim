5a355ff836945d7b6eb638d1fc91e8b581ed34c6
Plump Albert <plumpalbert@gmail.com>
2024-03-05 10:00:47 +0300

    updated lsp keys

--- lua/neowim/plugins/lspsaga.lua
+++ lua/neowim/plugins/lspsaga.lua
@@ -1,16 +1,40 @@
 return {
-	'nvimdev/lspsaga.nvim',
+	"nvimdev/lspsaga.nvim",
 	event = "LspAttach",
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
-		"nvim-tree/nvim-web-devicons"
+		"nvim-tree/nvim-web-devicons",
+	},
+	keys = {
+		{
+			"<leader>la",
+			":Lspsaga code_action<CR>",
+			desc = "Code actions",
+		},
+		{
+			"<leader>lk",
+			":Lspsaga diagnostic_jump_prev<CR>",
+			desc = "Previous diagnostic",
+		},
+		{
+			"<leader>lj",
+			":Lspsaga diagnostic_jump_next<CR>",
+			desc = "Next diagnostic",
+		},
+		{
+			"<F2>",
+			":Lspsaga rename<CR>",
+			desc = "Rename",
+		},
 	},
 	config = function()
-		require('lspsaga').setup({
+		require("lspsaga").setup({
 			symbol_in_winbar = {
 				enable = true,
-			}
+			},
+			implement = {
+				enable = true,
+			},
 		})
-	end
+	end,
 }
-
