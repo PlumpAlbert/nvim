909d88a1d2dd16ae69d7b2b281856f7b7d61b3ff
Plump Albert <plumpalbert@gmail.com>
2024-03-27 12:21:16 +0300

    fixed mapping for replasing surround

--- lua/neowim/plugins/mini-surround.lua
+++ lua/neowim/plugins/mini-surround.lua
@@ -5,12 +5,12 @@ return {
 	config = true,
 	opts = {
 		mappings = {
-			add = "gza", -- Add surrounding in Normal and Visual modes
+			add = "gza",   -- Add surrounding in Normal and Visual modes
 			delete = "gzd", -- Delete surrounding
-			find = "gzf", -- Find surrounding (to the right)
+			find = "gzf",  -- Find surrounding (to the right)
 			find_left = "gzF", -- Find surrounding (to the left)
 			highlight = "gzh", -- Highlight surrounding
-			replace = "gsr", -- Replace surrounding
+			replace = "gzr", -- Replace surrounding
 			update_n_lines = "gzn", -- Update `n_lines`
 
 			suffix_last = "l", -- Suffix to search with "prev" method
