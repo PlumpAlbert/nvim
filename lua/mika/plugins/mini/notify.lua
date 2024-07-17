05c659f110b90ef0a7e3866ad274a7ded4d84c85
Plump Albert <plumpalbert@gmail.com>
2024-07-17 09:01:56 +0300

    updated location of mini.notify

--- lua/mika/plugins/mini/notify.lua
+++ lua/mika/plugins/mini/notify.lua
@@ -6,21 +6,17 @@ return {
 		require("mini.notify").setup({
 			lsp_progress = {
 				enable = true,
-				duration_last = 1000,
+				duration_last = 500,
 			},
 			window = {
 				config = function()
-					return {
-						col = 2,
-						row = vim.o.lines - 6,
-					}
+					return { anchor = "SE" }
 				end,
-				max_width_share = 0.7,
-				winblend = 0,
+				max_width_share = 0.382,
+				winblend = 25,
 			},
 		})
 
 		vim.notify = require("mini.notify").make_notify()
 	end,
 }
-
