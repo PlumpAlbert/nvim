696eac81bb4e33f8b3db052e8a69e6f438a097c1
Plump Albert <plumpalbert@gmail.com>
2024-07-23 23:18:23 +0300

    added rose-pine theme

---
+++ lua/mika/plugins/themes/rose-pine.lua
@@ -0,0 +1,12 @@
+return {
+	"rose-pine/neovim",
+	name = "rose-pine",
+	event = "VeryLazy",
+	config = true,
+	priority = 1000,
+	opts = {
+		variant = "auto",
+		dark_variant = "main",
+		extend_background_behind_borders = true,
+	},
+}
