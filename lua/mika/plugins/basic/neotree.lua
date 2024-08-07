5178ef9d2f3e46671ab3a2c423136847049d9188
Matthew Fedin <plumpalbert@gmail.com>
2024-08-07 21:20:03 +0300

    moved window-picker into separate file

--- lua/mika/plugins/basic/neotree.lua
+++ lua/mika/plugins/basic/neotree.lua
@@ -7,26 +7,7 @@ return {
 		"nvim-lua/plenary.nvim",
 		"echasnovski/mini.icons",
 		"MunifTanjim/nui.nvim",
-		{
-			"s1n7ax/nvim-window-picker",
-			name = "window-picker",
-			version = "2.*",
-			config = true,
-			opts = {
-				hint = "statusline-winbar",
-				picker_config = {
-					statusline_winbar_picker = { use_winbar = "smart" },
-				},
-				filter_rules = {
-					autoselect_one = true,
-					include_current_win = false,
-					bo = {
-						filetype = { "neo-tree", "neo-tree-group", "notify" },
-						buftype = { "terminal", "quickfix" },
-					},
-				},
-			},
-		},
+		"s1n7ax/nvim-window-picker",
 	},
 	keys = {
 		{
