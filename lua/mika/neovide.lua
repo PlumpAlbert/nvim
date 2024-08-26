130e47ac918c7f75bc29a0ac3827348851ad019e
Plump Albert <plumpalbert@gmail.com>
2024-08-26 18:29:25 +0300

    created configuration for neovide

---
+++ lua/mika/neovide.lua
@@ -0,0 +1,9 @@
+if not vim.g.neovide then
+	return
+end
+
+vim.o.guifont = "Iosevka,Symbols_Nerd_Font:h14"
+vim.g.neovide_transparency = 0.95
+vim.g.neovide_hide_mouse_when_typing = true
+vim.g.neovide_cursor_vfx_mode = "wireframe"
+vim.g.neovide_maximized = true
