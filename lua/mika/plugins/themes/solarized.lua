eed9f70121431ae3f4d83243800073cb96290dfe
Plump Albert <plumpalbert@gmail.com>
2024-08-29 17:46:58 +0300

    added solarized theme

---
+++ lua/mika/plugins/themes/solarized.lua
@@ -0,0 +1,17 @@
+return {
+	"maxmx03/solarized.nvim",
+	lazy = false,
+	priority = 1000,
+	config = true,
+	opts = {
+		palette = "selenized",
+		transparent = {
+			enabled = true,
+			normal = false,
+			normalfloat = true,
+			whichkey = true,
+			lazy = true,
+			neotree = true,
+		},
+	},
+}
