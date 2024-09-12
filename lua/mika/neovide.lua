34ea80fce65f5d1446889cbf6c53fd52ed5fd6e7
Матвей Федин <Matvey.Fedin_ext@lemanapro.ru>
2024-09-12 12:12:10 +0300

    added neovide keymaps

--- lua/mika/neovide.lua
+++ lua/mika/neovide.lua
@@ -2,8 +2,29 @@ if not vim.g.neovide then
 	return
 end
 
-vim.o.guifont = "Iosevka,Symbols_Nerd_Font:h14"
-vim.g.neovide_transparency = 0.95
+local size = 14
+
+local function get_font()
+	return "Iosevka,Symbols_Nerd_Font:h" .. size
+end
+
+vim.o.guifont = get_font()
+vim.g.neovide_transparency = 0.92
 vim.g.neovide_hide_mouse_when_typing = true
 vim.g.neovide_cursor_vfx_mode = "wireframe"
 vim.g.neovide_maximized = true
+
+-- Clipboard management
+vim.keymap.set("i", "<C-S-v>", "<C-r>+", { desc = "Paste from clipboard" })
+vim.keymap.set("c", "<C-S-v>", "<C-r>+", { desc = "Paste from clipboard" })
+
+-- font change
+vim.keymap.set({ "n", "i" }, "<C-=>", function()
+	size = size + 1
+	vim.o.guifont = get_font()
+end, { desc = "Increase font size" })
+
+vim.keymap.set({ "n", "i" }, "<C-->", function()
+	size = size - 1
+	vim.o.guifont = get_font()
+end, { desc = "Increase font size" })
