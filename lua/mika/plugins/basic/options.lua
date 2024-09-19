0c3af91a63c3d4106ce4660e214523e3ac82558f
Матвей Федин <Matvey.Fedin_ext@lemanapro.ru>
2024-09-19 09:28:29 +0300

    updated plugin spec

--- lua/mika/plugins/basic/options.lua
+++ lua/mika/plugins/basic/options.lua
@@ -1,5 +1,5 @@
 return {
-	url = "https://git.plumpalbert.xyz/plump_albert/options.nvim",
+	url = "https://git.plumpalbert.xyz/akadmin/options.nvim",
 	dependencies = { "folke/neoconf.nvim" },
 	lazy = false,
 	config = true,
