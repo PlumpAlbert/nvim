aa8a8fc2ad49a40f994c357632be813a84e6d69c
Matthew Fedin <plumpalbert@gmail.com>
2024-10-05 15:44:10 +0300

    📝 Add Neoconf Configuration - Include the configuration for the `folke/neoconf.nvim` plugin - Set options to enable live reload and specific language servers (LSP) configurations

---
+++ lua/plumpalbert/base/neoconf.lua
@@ -0,0 +1,25 @@
+local M = {
+	"folke/neoconf.nvim",
+	lazy = false,
+	priority = 100,
+	config = true,
+}
+
+M.opts = {
+	live_reload = true,
+	plugins = {
+		lspconfig = {
+			enabled = true,
+		},
+		jsonls = {
+			enabled = true,
+			configured_servers_only = false,
+		},
+		lua_ls = {
+			enabled = false,
+			enabled_for_neovim_config = true,
+		},
+	},
+}
+
+return M
