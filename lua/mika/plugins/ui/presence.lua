53523f75f70f7333a0c1729c0209aaeeaa4e576c
Matthew Fedin <plumpalbert@gmail.com>
2024-10-27 19:17:04 +0300

    added discord presence plugin

---
+++ lua/mika/plugins/ui/presence.lua
@@ -0,0 +1,11 @@
+return {
+	"andweeb/presence.nvim",
+	event = "VeryLazy",
+	config = true,
+	opts = {
+		enable_line_number = true,
+		debounce_timeout = 10,
+		buttons = true,
+		show_time = true,
+	},
+}
