c0093edd9e7d11228dd21282f19312fbd9275fd0
Matthew Fedin <plumpalbert@gmail.com>
2024-11-04 23:23:03 +0300

    added keymap to change colorscheme at random

--- lua/mika/keymaps.lua
+++ lua/mika/keymaps.lua
@@ -1,2 +1,11 @@
 vim.keymap.set({ "n", "v" }, "Y", '"+y', { desc = "Copy to system clipboard" })
 vim.keymap.set({ "n", "i" }, "<C-s>", "<esc>:wa<cr>", { desc = "Save changes" })
+
+vim.keymap.set({ "n", "i", "v" }, "<F5>", function()
+	local colorschemes = require("mika.utils").get_colorschemes()
+
+	local index = math.random(1, #colorschemes)
+
+	vim.notify("Colorscheme: " .. colorschemes[index])
+	vim.cmd("colorscheme " .. colorschemes[index])
+end)
