3829b6732e6731eac883f1e2118413020cde37a2
Matthew Fedin <plumpalbert@gmail.com>
2024-11-19 09:19:57 +0300

    enable auto transparentize inside neoconf

--- lua/mika/plugins/ui/transparent.lua
+++ lua/mika/plugins/ui/transparent.lua
@@ -1,8 +1,30 @@
 return {
 	"xiyaowong/transparent.nvim",
+	lazy = false,
+	dependencies = {
+		"folke/neoconf.nvim",
+	},
 	config = function()
 		require("transparent").setup()
 		require("transparent").clear_prefix("NeoTree")
+
+		local config = require("neoconf").get("transparent", {
+			enabled = false,
+		})
+		if config.enabled then
+			vim.cmd("TransparentEnable")
+		end
+	end,
+	init = function()
+		require("neoconf.plugins").register({
+			name = "transparent",
+			on_schema = function(schema)
+				schema:set("transparent.enabled", {
+					type = "boolean",
+					description = "Enable transparent background",
+				})
+			end,
+		})
 	end,
 	cmd = {
 		"TransparentEnable",
