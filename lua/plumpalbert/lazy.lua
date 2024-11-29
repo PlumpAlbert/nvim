20f03888fa9de4bbfcd44b81a8aa3f109eedb509
Matthew Fedin <plumpalbert@gmail.com>
2024-11-29 14:48:01 +0300

    added base16 themes

--- lua/plumpalbert/lazy.lua
+++ lua/plumpalbert/lazy.lua
@@ -35,6 +35,7 @@ require("lazy").setup({
 		{ import = "plumpalbert.base" },
 		{ import = "plumpalbert.lsp" },
 		{ import = "plumpalbert.mini" },
+		{ import = "plumpalbert.themes" },
 	},
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
---
+++ lua/plumpalbert/themes/base16.lua
@@ -0,0 +1,5 @@
+return {
+	"RRethy/base16-nvim",
+	lazy = false,
+	priority = 1000,
+}
