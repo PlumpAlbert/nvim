f955a0d6ccbab170447be699a38ca8bc4a496e1d
Matthew Fedin <plumpalbert@gmail.com>
2024-11-29 17:57:41 +0300

    updated completion configuration

--- lua/plumpalbert/lsp/completion.lua
+++ lua/plumpalbert/lsp/completion.lua
@@ -10,8 +10,12 @@ return {
 		{ 'saadparwaiz1/cmp_luasnip' },
 		{ 'folke/lazydev.nvim' },
 	},
+	init = function()
+		vim.o.completeopt = 'menu,menuone,noselect'
+	end,
 	config = function ()
 		local cmp = require("cmp")
+
 		cmp.setup({
 			snippet = {
 				expand = function(args)
@@ -22,15 +26,33 @@ return {
 				['<C-b>'] = cmp.mapping.scroll_docs(-4),
 				['<C-f>'] = cmp.mapping.scroll_docs(4),
 				['<C-e>'] = cmp.mapping.abort(),
-				['<CR>'] = cmp.mapping.confirm({ select = true })
+				['<C-n>'] = cmp.mapping.select_next_item({
+					behavior = cmp.SelectBehavior.Select
+				}),
+				['<C-p>'] = cmp.mapping.select_prev_item({
+					behavior = cmp.SelectBehavior.Select
+				}),
+				['<C-y>'] = cmp.mapping.confirm({
+					select = true,
+					behavior = cmp.ConfirmBehavior.Replace,
+				}),
+				['<CR>'] = cmp.mapping.confirm({
+					select = true,
+					behavior = cmp.ConfirmBehavior.Replace,
+				})
 			}),
 			sources = cmp.config.sources({
 				{name = 'lazydev', group_index = 0},
-				{name = 'nvim_lsp'},
-				{name = 'luasnip'},
-			}, {
-				{name= 'buffer'}
-			})
+				{name = 'nvim_lsp', group_index = 1},
+				{name = 'luasnip', max_item_count = 10},
+				{name= 'buffer', keyword_length = 3, group_index = 99}
+			}),
+			performance = {
+				max_view_entries = 10,
+			},
+			experimental = {
+				ghost_text = true
+			}
 		})
 
 		cmp.setup.cmdline(':', {
@@ -42,6 +64,9 @@ return {
 			}),
 			matching = {
 				disallow_symbol_nonprefix_matching = false
+			},
+			performance = {
+				max_view_entries = 5,
 			}
 		})
 
