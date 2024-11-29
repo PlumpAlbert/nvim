a083202c3274ce91611734b34d2201cf3e1cd299
Matthew Fedin <plumpalbert@gmail.com>
2024-11-29 14:47:43 +0300

    include mini plugins

--- lua/plumpalbert/lazy.lua
+++ lua/plumpalbert/lazy.lua
@@ -34,6 +34,7 @@ require("lazy").setup({
 		-- import your plugins
 		{ import = "plumpalbert.base" },
 		{ import = "plumpalbert.lsp" },
+		{ import = "plumpalbert.mini" },
 	},
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
