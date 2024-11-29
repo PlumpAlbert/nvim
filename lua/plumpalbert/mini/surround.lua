e78350ff639a3905959a468c7f29679b39ef9337
Matthew Fedin <plumpalbert@gmail.com>
2024-11-29 14:46:53 +0300

    added surround plugin

---
+++ lua/plumpalbert/mini/surround.lua
@@ -0,0 +1,19 @@
+return {
+	"echasnovski/mini.surround",
+	version = "*",
+	event = "VeryLazy",
+	config = true,
+	opts = {
+		mappings = {
+			add = "gza",
+			delete = "gzd",
+			find = "gzf",
+			find_left = "gzF",
+			highlight = "gzh",
+			replace = "gzr",
+			unpdate_n_lines = "gzn",
+			suffix_last = "l",
+			suffix_next = "n",
+		},
+	},
+}
