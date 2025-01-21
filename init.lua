766a2f41bdb7c76777b80f87161a1a95af3bb87b
Matthew Fedin <plumpalbert@gmail.com>
2025-01-21 19:48:16 +0300

    updated initial config

--- init.lua
+++ init.lua
@@ -6,6 +6,8 @@ vim.g.have_nerd_font = true
 vim.opt.mouse = 'a'
 vim.opt.number = true
 vim.opt.showmode = false
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 0
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
@@ -19,10 +21,10 @@ vim.opt.splitbelow = true
 
 vim.opt.list = true
 vim.opt.listchars = {
-	tab = '-->',
-	trail = '␣',
-	nbsp = '␣',
-	space = '·'
+  tab = '-->',
+  trail = '␣',
+  nbsp = '␣',
+  space = '·',
 }
 
 vim.opt.inccommand = 'split'
@@ -32,15 +34,15 @@ vim.opt.scrolloff = 3
 --#region Keymaps
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set({'n', 'v'}, 'Y', '"+y')
+vim.keymap.set({ 'n', 'v' }, 'Y', '"+y')
 --#endregion
 
 vim.api.nvim_create_autocmd('TextYankPost', {
-	desc = 'Highlight yanked text',
-	group = vim.api.nvim_create_augroup('plumpvim-highlight-yank', {clear=true}),
-	callback = function()
-		vim.highlight.on_yank()
-	end
+  desc = 'Highlight yanked text',
+  group = vim.api.nvim_create_augroup('plumpvim-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
 
 --#region Lazy
@@ -54,23 +56,23 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
-	spec = {
-		{ import = 'plumpvim.plugins' },
-	},
-	install = { colorscheme = { 'retrobox', 'habamax', 'default' } },
-	checker = { enabled = false },
-	change_detection = { enabled = false },
-	defaults = { 
-		lazy = true,
-		config = true,
-	},
-	lockfile = vim.fn.stdpath('cache') .. "/lazy-lock.json",
-	ui = {
-		size = { width = 1, height = 1 },
-		border = "none",
-		wrap = false,
-		backdrop = 0,
-	},
-})
+require('lazy').setup {
+  spec = {
+    { import = 'plumpvim.plugins' },
+  },
+  install = { colorscheme = { 'retrobox', 'habamax', 'default' } },
+  checker = { enabled = false },
+  change_detection = { enabled = false },
+  defaults = {
+    lazy = true,
+    config = true,
+  },
+  lockfile = vim.fn.stdpath 'cache' .. '/lazy-lock.json',
+  ui = {
+    size = { width = 1, height = 1 },
+    border = 'none',
+    wrap = false,
+    backdrop = 0,
+  },
+}
 --#endregion
