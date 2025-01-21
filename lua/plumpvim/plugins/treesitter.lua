5f9b756feaa65e4fea70cd7f3505bb4eb25ed6bd
Matthew Fedin <plumpalbert@gmail.com>
2025-01-21 19:48:10 +0300

    added treesitter support

---
+++ lua/plumpvim/plugins/treesitter.lua
@@ -0,0 +1,62 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    main = 'nvim-treesitter.configs',
+    lazy = false,
+    opts = {
+      ensure_installed = { 'markdown', 'markdown_inline', 'lua', 'bash' },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { 'ruby' },
+      },
+      indent = { enable = true, disable = { 'ruby' } },
+    },
+  },
+  {
+    'windwp/nvim-ts-autotag',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    lazy = false,
+    opts = {
+      opts = {
+        enable_close = true,
+        enable_rename = true,
+        enable_close_on_slash = false,
+      },
+    },
+  },
+  {
+    'HiPhish/rainbow-delimiters.nvim',
+    lazy = false,
+    init = function()
+      local rainbow_delimiters = require 'rainbow-delimiters'
+
+      vim.g.rainbow_delimiters = {
+        strategy = {
+          [''] = rainbow_delimiters.strategy['global'],
+          vim = rainbow_delimiters.strategy['local'],
+        },
+        query = {
+          [''] = 'rainbow-delimiters',
+          lua = 'rainbow-blocks',
+          latex = 'rainbow-blocks',
+        },
+        priority = {
+          [''] = 110,
+          lua = 210,
+          latex = 210,
+        },
+        highlight = {
+          'RainbowDelimiterRed',
+          'RainbowDelimiterYellow',
+          'RainbowDelimiterBlue',
+          'RainbowDelimiterOrange',
+          'RainbowDelimiterGreen',
+          'RainbowDelimiterViolet',
+          'RainbowDelimiterCyan',
+        },
+      }
+    end,
+  },
+}
