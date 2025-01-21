20775d23aa316b2789008599968c67dbac273e4e
Matthew Fedin <plumpalbert@gmail.com>
2025-01-21 18:22:28 +0300

    added folding for keymaps

--- init.lua
+++ init.lua
@@ -29,7 +29,12 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 vim.opt.scrolloff = 3
 
+--#region Keymaps
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set({'n', 'v'}, 'Y', '"+y')
+--#endregion
+
 vim.api.nvim_create_autocmd('TextYankPost', {
 	desc = 'Highlight yanked text',
 	group = vim.api.nvim_create_augroup('plumpvim-highlight-yank', {clear=true}),
@@ -37,3 +42,34 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 		vim.highlight.on_yank()
 	end
 })
+
+--#region Lazy
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+	spec = {
+		{ import = 'plumpvim.plugins' },
+	},
+	install = { colorscheme = { 'retrobox', 'habamax', 'default' } },
+	checker = { enabled = false },
+	defaults = { 
+		lazy = true,
+		config = true,
+	},
+	lockfile = vim.fn.stdpath('cache') .. "/lazy-lock.json",
+	ui = {
+		size = { width = 1, height = 1 },
+		border = "none",
+		wrap = false,
+		backdrop = 0,
+	},
+})
+--#endregion
