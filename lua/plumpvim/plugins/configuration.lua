71b3fb8ed38e506666778b27ee542b4c0d08e902
Матвей Федин <Matvey.Fedin_ext@lemanapro.ru>
2025-01-24 09:17:26 +0300

    added silicon plugin

--- lua/plumpvim/plugins/configuration.lua
+++ lua/plumpvim/plugins/configuration.lua
@@ -1,3 +1,19 @@
+---Trim filepath
+---@param path string
+local function trim_filepath(path)
+  local split = vim.split(path, '/', {
+    trimempty = true,
+    plain = true,
+  })
+  local parents = vim.list_slice(split, 1, #split - 1)
+
+  for i = 1, #parents do
+    parents[i] = parents[i]:sub(1, 2)
+  end
+
+  return string.format('%s/%s', table.concat(parents, '/'), split[#split])
+end
+
 return {
   {
     'PlumpAlbert/options.nvim',
@@ -12,5 +28,73 @@ return {
   {
     'wakatime/vim-wakatime',
     lazy = false,
+  },
+  {
+    'michaelrommel/nvim-silicon',
+    lazy = true,
+    cmd = 'Silicon',
+    keys = {
+      {
+        '<leader>s',
+        ':Silicon<CR>',
+        desc = 'Code snippet',
+      },
+      {
+        '<leader>s',
+        ":'<,'>Silicon<CR>",
+        desc = 'Code snippet',
+        mode = { 'v' },
+      },
+    },
+    opts = {
+      theme = 'gruvbox-dark',
+      background = '#076678',
+      pad_horiz = 80,
+      pad_vert = 40,
+      no_round_corner = true,
+      no_window_controls = true,
+      no_line_number = false,
+      line_offset = function(args)
+        if args.range == 1 then
+          return args.line1
+        end
+        return 1
+      end,
+      line_pad = 0,
+      tab_width = 2,
+      language = function()
+        local ft = vim.bo.filetype
+
+        if ft == 'jsonc' then
+          return 'json'
+        end
+
+        return ft
+      end,
+      shadow_blur_radius = 24,
+      shadow_offset_x = 8,
+      shadow_offset_y = 8,
+      shadow_color = '#100808',
+      gobble = true,
+      to_clipboard = true,
+      window_title = function()
+        local cwd = vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
+        local relative_path = vim.fn.expand '%:.'
+
+        local max_width = vim.o.textwidth
+        if max_width == 0 then
+          max_width = 80
+        end
+
+        if string.len(relative_path) > max_width - string.len(cwd) then
+          relative_path = trim_filepath(relative_path)
+        end
+
+        return string.format('[%s] %s', cwd, relative_path)
+      end,
+      wslclipboard = 'auto',
+      wslclipboardcopy = 'delete',
+      command = 'silicon',
+    },
   },
 }
