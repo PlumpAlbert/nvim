0011d24824fc866cdf44d0c72b1d9922d10413bc
Матвей Федин <Matvey.Fedin_ext@lemanapro.ru>
2025-01-24 14:43:51 +0300

    updated truncate options

--- lua/plumpvim/plugins/mini.lua
+++ lua/plumpvim/plugins/mini.lua
@@ -117,9 +117,9 @@ return {
       content = {
         active = function()
           local mode, mode_hl = MiniStatusline.section_mode { trunc_width = 120 }
-          local git = MiniStatusline.section_git { trunc_width = 40 }
-          local filename = MiniStatusline.section_filename { trunc_width = 30 }
-          local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 30 }
+          local git = MiniStatusline.section_git { trunc_width = 10000 }
+          local filename = MiniStatusline.section_filename { trunc_width = 10000 }
+          local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 10000 }
 
           return MiniStatusline.combine_groups {
             { hl = mode_hl, strings = { mode } },
