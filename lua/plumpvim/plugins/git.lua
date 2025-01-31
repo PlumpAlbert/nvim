4d085a5617fe5484f1ddb23ec5c081304c0a7168
Matthew Fedin <plumpalbert@gmail.com>
2025-01-31 16:43:37 +0300

    [git] updated line blame format

--- lua/plumpvim/plugins/git.lua
+++ lua/plumpvim/plugins/git.lua
@@ -1,4 +1,5 @@
 return {
+  -- lewis6991/gitsigns.nvim
   {
     'lewis6991/gitsigns.nvim',
     event = { 'BufEnter' },
@@ -14,7 +15,7 @@ return {
         changedelete = { text = '~' },
       },
       current_line_blame = true,
-      current_line_blame_formatter = '<author> 󰁥 <summary>',
+      current_line_blame_formatter = '(<author>)@ <summary>',
       current_line_blame_opts = {
         virt_text = true,
         virt_text_pos = 'eol',
