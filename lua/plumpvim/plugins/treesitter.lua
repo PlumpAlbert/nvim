a57a94ee6a951e06170a132bbe1becce5b8497ed
Matthew Fedin <plumpalbert@gmail.com>
2025-02-24 19:25:26 +0300

    added ts-comments.nvim

--- lua/plumpvim/plugins/treesitter.lua
+++ lua/plumpvim/plugins/treesitter.lua
@@ -67,4 +67,11 @@ return {
       }
     end,
   },
+  -- folke/ts-comments.nvim
+  {
+    'folke/ts-comments.nvim',
+    opts = {},
+    event = 'VeryLazy',
+    enabled = vim.fn.has 'nvim-0.10.0' == 1,
+  },
 }
