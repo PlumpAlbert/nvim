5a6ed09a266faa5ad193761155da44ac8fd28092
Plump Albert <plumpalbert@gmail.com>
2025-05-19 09:40:39 +0300

    [LSP] updated diagnostics icons

--- lua/plumpvim/plugins/lsp/lspconfig.lua
+++ lua/plumpvim/plugins/lsp/lspconfig.lua
@@ -6,10 +6,10 @@ local diagnostic_config = {
 	severity_sort = true,
 	signs = {
 		text = {
-			[vim.diagnostic.severity.ERROR] = '󰃤',
-			[vim.diagnostic.severity.WARN] = '󱈸',
-			[vim.diagnostic.severity.INFO] = '󰙎',
-			[vim.diagnostic.severity.HINT] = '',
+			[vim.diagnostic.severity.ERROR] = ' ',
+			[vim.diagnostic.severity.WARN] = ' ',
+			[vim.diagnostic.severity.INFO] = ' ',
+			[vim.diagnostic.severity.HINT] = '󰌵',
 		},
 	},
 }
