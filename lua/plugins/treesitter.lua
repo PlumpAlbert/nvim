6323f3101c2cddf4f84f476ffc13252ef8457cbf
Matthew Fedin <plumpalbert@gmail.com>
2026-09-18 04:28:29 +0300

    Update treesitter setup for Neovim 0.13

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,30 +1,53 @@
+local parsers = {
+  "bash",
+  "c",
+  "css",
+  "go",
+  "html",
+  "javascript",
+  "json",
+  "lua",
+  "markdown",
+  "markdown_inline",
+  "python",
+  "rust",
+  "tsx",
+  "typescript",
+  "vim",
+  "yaml",
+}
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    branch = "master",
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
     config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "bash",
-          "c",
-          "css",
-          "go",
-          "html",
-          "javascript",
-          "json",
-          "lua",
-          "markdown",
-          "python",
-          "rust",
-          "tsx",
-          "typescript",
-          "vim",
-          "yaml",
-        },
-        highlight = { enable = true },
-        indent = { enable = true },
+      local treesitter = require("nvim-treesitter")
+
+      treesitter.setup()
+
+      local installed = {}
+      for _, parser in ipairs(treesitter.get_installed("parsers")) do
+        installed[parser] = true
+      end
+
+      local missing = vim.tbl_filter(function(parser)
+        return not installed[parser]
+      end, parsers)
+
+      if #missing > 0 then
+        treesitter.install(missing)
+      end
+
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("NewvimTreesitter", { clear = true }),
+        callback = function(event)
+          if pcall(vim.treesitter.start, event.buf) then
+            vim.bo[event.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
       })
     end,
   },
