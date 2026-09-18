1e2902ed78f62c9b964904edb929f0ead6ad7bd3
Matthew Fedin <plumpalbert@gmail.com>
2026-09-18 05:11:53 +0300

    Disable lazy update notifications

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,6 +14,6 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   { import = "plugins" },
 }, {
-  checker = { enabled = true },
+  checker = { enabled = false },
   change_detection = { notify = false },
 })
