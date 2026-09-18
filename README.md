81bba545ef91feebda42ef56728867e91880c762
Matthew Fedin <plumpalbert@gmail.com>
2026-09-18 04:45:26 +0300

    Add rendered markdown UI

--- README.md
+++ README.md
@@ -45,6 +45,7 @@ set -g focus-events on
 - Completion: `nvim-cmp` с LSP/path/buffer/snippet sources.
 - Snippets: `LuaSnip` + `friendly-snippets`.
 - Editing QoL: `mini.ai`, `mini.surround`, `mini.comment`, `mini.pairs`.
+- Markdown UI: `render-markdown.nvim`.
 - fzf: `fzf-lua` для файлов, grep, буферов, help и `vim.ui.select`.
 - Keymap hints: `which-key.nvim` для подсказок по `<leader>` mappings.
 - Tmux/split navigation: `smart-splits.nvim` с tmux integration.
---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      file_types = { "markdown" },
+      heading = {
+        icons = { "󰎤 ", "󰎧 ", "󰎪 ", "󰎭 ", "󰎱 ", "󰎳 " },
+      },
+      checkbox = {
+        enabled = true,
+      },
+      code = {
+        border = "thin",
+      },
+    },
+  },
+}
